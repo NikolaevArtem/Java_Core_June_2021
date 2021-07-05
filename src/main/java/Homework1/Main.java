dba34ba24dc6593005079fea06dfc33ddd170d47
Frank Fibonacci <fadedfacet@gmail.com>
2021-07-05 21:29:35 +0300

    homework update

--- src/main/java/Homework1/Main.java
+++ src/main/java/Homework1/Main.java
@@ -1,4 +1,14 @@
 package Homework1;
 
 public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (!args[i].equals("ошибка")) {
+                System.out.println(args[i] + ": " + args[i].length());
+            } else {
+                System.err.println("тревога!");
+                System.exit(0);
+            }
+    }
+}
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,7 @@
 public class Main {
 
   public static void main(String[] args) {
-    for (int i = 0; i < args.length; i++) {
-      if (!args[i].equals("ошибка")) {
-        System.out.println(args[i] + ": " + args[i].length());
-      } else {
-        System.err.println("тревога!");
-        System.exit(0);
-      }
+
 
     }
   }
