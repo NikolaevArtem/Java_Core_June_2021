a13ba2b1a5d7d650a447907e877a8f5f8fc001c4
Frank Fibonacci <fadedfacet@gmail.com>
2021-07-05 20:05:17 +0300

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,11 @@ public class Main {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello, Epam! :)");
+    int i = 0;
+    while (i < 3) {
+      System.out.println("Hello, Epam! :)");
+      i++;
+    }
 
   }
 }
