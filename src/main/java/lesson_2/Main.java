d153aa2af84c89e3a89f21fee82ae85e5aff1074
Elena Rog <rog.elena.yu@gmail.com>
2021-07-05 20:39:56 +0300

    2st commit

--- src/main/java/lesson_2/Main.java
+++ src/main/java/lesson_2/Main.java
@@ -3,9 +3,8 @@ package lesson_2;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("Hello, elenarog!");
 
-    System.out.println("Hello World2!");
   }
 
 }
