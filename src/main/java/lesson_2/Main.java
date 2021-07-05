67946e1f93a7a8fe2c0c0d1aa74b08a7ac3c401d
Artem Nikolaev <anikolaev@ebsco.com>
2021-07-06 00:14:56 +0500

    Test push

--- src/main/java/lesson_2/Main.java
+++ src/main/java/lesson_2/Main.java
@@ -1,10 +1,12 @@
 package lesson_2;
 
+import lesson_2.catbox.Cat1;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
-
+    final int x = 5;
+    final Cat1 cat1 = new Cat1();
   }
 
 }
