e415e2951e92f50ae7ce99ba56e5bd5b7b5717d2
Gamzat Asadulaev <asadulaevgamzat@gmail.com>
2021-07-05 20:07:49 +0300

    Test commit 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello, Russia!");
+    System.out.println("Hello, World!");
+
+    System.out.println("Hello World2!");
   }
 
 }
