0248aeb6b749736dec3fe10886128a393d8e55ac
Artem Nikolaev <anikolaev@ebsco.com>
2021-07-05 21:47:34 +0500

    Initial commit v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
 public class Main {
 
   public static void main(String[] args) {
-
-    int i = 0;
-    while (i < 3) {
-      System.out.println("Hello, Epam!;)");
-      i++;
-    }
+    System.out.println("Hello, World!");
   }
 
 }
