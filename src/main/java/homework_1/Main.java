4e4b7d97ad76c5f2e0e2c27de0c33ea77be1d3c9
LinaFawn <tcipkoalina2000@mail.ru>
2021-07-07 02:17:59 +0300

    hm_1_v2

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -2,10 +2,16 @@ package homework_1;
 
 public class Main {
 
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
-        for(String a: args) {
-            if (a.equals("ошибка"))
+
+         for(String a: args) {
+            if (a.equals("error")) {
+                System.out.println(ANSI_RED + "Alarm!" + ANSI_RESET);
                 break;
+            }
             System.out.println(a + ": " + a.length());
         }
 
