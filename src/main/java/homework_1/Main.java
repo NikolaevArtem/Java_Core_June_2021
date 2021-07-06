212b306aa28ef4df3042f85b309dd7135abda59e
Nikita <sotheres@gmail.com>
2021-07-06 11:22:25 +0300

    Homework 1

---
+++ src/main/java/homework_1/Main.java
@@ -0,0 +1,14 @@
+package homework_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            if (arg.equals("error")) {
+                System.err.println("Alarm!");
+                break;
+            }
+            System.out.println(arg + ": " + arg.length());
+        }
+    }
+}
