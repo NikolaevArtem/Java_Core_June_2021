592a77055a6d3701a46472c8326cf8062469360b
Konstantin Ivanov <kostya-ivanov9623@mail.ru>
2021-07-06 21:32:33 +0300

    first_homework

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello, World!");
     System.out.println("Nice");
+    System.err.println("as");
   }
 
 }
