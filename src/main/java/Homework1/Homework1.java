bf42e59e8896b077c7f516336442201fb56d0ddc
Frank Fibonacci <fadedfacet@gmail.com>
2021-07-06 16:50:46 +0300

    upd

--- src/main/java/Homework1/Homework1.java
+++ src/main/java/Homework1/Homework1.java
@@ -1,18 +1,23 @@
 package Homework1;
 
 public class Homework1 {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+
     public static void main(String[] args) {
         for (String arg : args) {
             if (arg.equals("ошибка")) {
-                System.out.println(ANSI_RED + "тревога!" + ANSI_RESET);
+                System.out.println(ANSI_RED + "Тревога!" + ANSI_RESET);
                 return;
             }
-            System.out.println(ANSI_YELLOW + arg + ANSI_RESET + ": " +
-                    ANSI_GREEN + arg.length() + ANSI_RESET);
+            int len = arg.length();
+            String add = "букв";
+            if (len == 1) {
+                add = "буква";
+            } else if (len > 1 && len < 5) {
+                add = "буквы";
+            }
+            System.out.println(arg + ": " + arg.length() + " " + add);
         }
     }
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_GREEN = "\u001B[32m";
 }
