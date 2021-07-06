17484b9af11895f24f2d05d81c6088dfbf1af6df
Konstantin Ivanov <kostya-ivanov9623@mail.ru>
2021-07-06 21:33:03 +0300

    first_homework

---
+++ src/main/java/homework_1/Main.java
@@ -0,0 +1,12 @@
+package homework_1;
+
+public class Main {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            if (arg.equals("ошибка")) {
+                System.err.println("ошибка");
+                break;
+            } System.out.println(arg + ":" + arg.length() + " букв");
+        }
+    }
+}
