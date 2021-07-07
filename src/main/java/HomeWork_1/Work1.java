3f5fd3f4f33b7e4903616dec2c27b10528ad977a
Kollega1984 <bomba_25@mail.ru>
2021-07-07 10:27:30 +0300

    homework_1.4

--- src/main/java/HomeWork_1/Work1.java
+++ src/main/java/HomeWork_1/Work1.java
@@ -3,13 +3,17 @@ package HomeWork_1;
 public class Work1 {
     public static void main(String[] args) {
 
-            if(args == null) return;
+            if(args == null || args[0].isEmpty()) return;
+
             for (String str: args){
-                if(str.equals("ошибка")){
-                    System.out.println("\u001B[31m"+"Тревога!");
-                    return;
-                }
-                System.out.println(str + ": " + str.length() + " букв");
+
+                    if(str.equals("ошибка")){
+                        System.out.println("\u001B[31m"+"Тревога!");
+                        return;
+                    }
+                    System.out.println(str + ": " + str.length() + " букв");
+
+
             }
 
 
