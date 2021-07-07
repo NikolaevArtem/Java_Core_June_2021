174a9baa68df3691eb030b5d8daf55905bdcef37
Dmitry Mozhaev <lightveider@gmail.com>
2021-07-07 15:57:44 +0300

    First Hometask v1.1

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -4,8 +4,11 @@ package homework_1;
 public class Main {
     public static void main(String[] args) {
 
-        for (String word : args){
-            if (word.equals("Error") | word.equals("error")) break;
+        for (String word : args) {
+            if (word.equals("Error") | word.equals("error")) {
+                System.out.println("Alarm");
+                break;
+            }
             System.out.println(word + ":" + word.length());
         }
     }
