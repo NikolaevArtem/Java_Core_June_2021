edb888f4f67dd3bae13cb4e8fdcd856c723d92b0
Allzza <allzza4279@gmail.com>
2021-07-07 21:38:05 +0300

    readme modified

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -2,8 +2,16 @@ package homework_1;
 
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello homework!");
-  }
+    public static void main(String[] args) {
+        for (String object : args) {
+            if (object.equals("ошибка")) {
+                System.out.println("Тревога");
+                break;
+            }
+            else {
+                System.out.println(object + ": " + object.length() + " букв");
+            }
+        }
 
+    }
 }
