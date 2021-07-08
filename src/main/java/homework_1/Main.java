e6d248c8a53e43bac8cb45b22efa0f740133be74
KhlebnikovEvgeniy <khebnikove@gmail.com>
2021-07-08 11:20:26 +0300

    init

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -2,9 +2,19 @@ package homework_1;
 
 public class Main {
 
-  public static void main(String[] args) {
+  private static void main(String[] args) {
     //Домашняя работа от 05.07.2021
-    System.out.println("Hello homework!");
-  }
+    if (args == null || args.length < 1) {
+      return;
+    }
 
+    for (String arg : args) {
+      if (arg.equals("error")) {
+        System.out.println(ConsoleColors.RED_BOLD + "Alarm!" + ConsoleColors.RESET);
+        break;
+      } else {
+        System.out.println(arg + ": " + arg.length() + " letters");
+      }
+    }
+  }
 }
