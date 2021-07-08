3bd39c09ca9b1bc2fefd50c8eb5f2d08f8a78b35
Dmitry Mozhaev <lightveider@gmail.com>
2021-07-08 18:21:24 +0300

    Homework reloaded

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -1,9 +1,15 @@
 package homework_1;
 
 public class Main {
+    public static void main(String[] args) {
 
-  public static void main(String[] args) {
-    System.out.println("Hello homework!");
-  }
-
+        for (String word : args) {
+            if (word.equals("Error") | word.equals("error")) {
+                System.out.println("Alarm");
+                break;
+            }
+            System.out.println(word + ":" + word.length());
+        }
+    }
 }
+
