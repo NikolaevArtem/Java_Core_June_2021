468f48e06216d77c02d0dd81772d9760e73c4e87
GabdrakhimovaRR <gabdrakhimovarr@gmail.com>
2021-07-10 01:44:21 +0300

    homework_1(2)

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -1,38 +1,17 @@
 package homework_1;
-import java.util.Scanner;
 
 public class Main {
   static final String RED = "\u001b[31m";
   static final String RESET = "\u001b[0m";
 
   public static void main(String[] args) {
-    try {
-      dialog();
-    } catch (AlarmException e) {
-      System.out.println(e.getMessage());
-    }
-  }
-
-  public static void dialog() throws AlarmException {
-    System.out.println("Enter arguments:");
-    Scanner console = new Scanner(System.in);
-
-    while (console.hasNext()) {
-      String word = console.next();
-
-      if (!word.equals("error")) {
-        int n = word.length();
-        System.out.println(word + ": " + n + " letters");
+      for (int i = 0; i < args.length; i++) {
+      if (args[i].equals("error")) {
+        System.out.println(RED + "Alarm!"+ RESET);
+        break;
       } else {
-        throw new AlarmException(RED + "Alarm!"+ RESET);
+        System.out.println(args[i] + ": " + args[i].length() + " letters");
       }
     }
-  };
-
-  public static class AlarmException extends Exception {
-    public AlarmException(String message) {
-      super(message);
-    }
   }
-
 }
