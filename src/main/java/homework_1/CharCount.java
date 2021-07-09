121d8e27f7793e48c5f17c9a6a1e6a6350d0f359
bttrthnubtch <slowly@live.ru>
2021-07-09 16:09:17 +0300

    Fix indentation

--- src/main/java/homework_1/CharCount.java
+++ src/main/java/homework_1/CharCount.java
@@ -2,20 +2,18 @@ package homework_1;
 
 public class CharCount {
     public static void main(String[] args) {
-        if (args.length == 0)
-        {
+        if (args.length == 0) {
             System.out.println("There is no parameters, exit program.");
-        }
-        else {
+        } else {
 
-        System.out.println("Count of Char's in words:");
-        for (String arg : args) {
-            if (arg.equals("error")) {
-                System.err.println("Alarm!");
-                break;
-            }
+            System.out.println("Count of Char's in words:");
+            for (String arg : args) {
+                if (arg.equals("error")) {
+                    System.err.println("Alarm!");
+                    break;
+                }
                 System.out.println(arg + ": " + arg.length());
-        }
+            }
         }
     }
 }
