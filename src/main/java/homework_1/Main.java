14a40a829a68e61ff2134ec85517ed27e5b7de03
roman <roman.s.popov@gmail.com>
2021-07-09 19:49:24 +0300

    homework 1 + readme

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -3,7 +3,16 @@ package homework_1;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello homework!");
+
+    for (String a : args) {
+      if (a.equals("error")) {
+        System.out.println("\u001B[31m" + "Error" + "\u001B[0m");
+        break;
+      }
+
+      System.out.println(a + ":" + a.length() + " letters");
+    }
+
   }
 
 }
