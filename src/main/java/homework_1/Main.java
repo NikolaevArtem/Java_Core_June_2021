be6891a4045d1dd2ccc7939baad868690afb90c3
ijustwannatry <dmitrisedam11@gmail.com>
2021-07-09 14:27:02 +0300

    Fixed version after the review

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -4,8 +4,8 @@ public class Main {
   public static void main(String[] args) {
     for (String s : args) {
       if ("error".equals(s)) {
-        System.out.println("\u001B[31mAlarm!\u001B[0m\n");
-        System.exit(0);
+        System.out.println("\u001B[31m" + "Alarm!" + "\u001B[0m\n");
+        break;
       } else {
         String letters = s.length() == 1 ? " letter" : " letters";
         System.out.println(s + ": " + s.length() + letters);
