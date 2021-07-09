76efae8538915e4ef3f9565ef672003287e018e4
Alexandr Kirshin <kirhin1@gmail.com>
2021-07-09 17:42:05 +0300

    changed

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -2,11 +2,13 @@ package homework_1;
 
 public class Main {
   public static final String ANSI_RED = "\u001B[31m"; // ANSI escape code. Text color: RED.
+  public static final String ANSI_RESET = "\u001B[0m"; // ANSI reset.
+
 
   public static void main(String[] args) {
     for(String str : args){
       if (str.equals("error")){
-        System.out.println(ANSI_RED + "Alarm!"); // Working on Ubuntu.
+        System.out.println(ANSI_RED + "Alarm!" + ANSI_RESET); // Working on Ubuntu.
         break;
       }
       if (str.length() >= 2) {
