c87c2cfec63207cf3502038d6d756700f093d153
Elena Rog <rog.elena.yu@gmail.com>
2021-07-10 18:19:18 +0300

    homework 1

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -1,9 +1,16 @@
 package homework_1;
 
 public class Main {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
-  public static void main(String[] args) {
-    System.out.println("Hello homework!");
-  }
-  11
+    public static void main(String[] args) {
+        for (String arg : args) {
+            if ("error".equalsIgnoreCase(arg) || "ошибка".equalsIgnoreCase(arg)) {
+                System.out.println(ANSI_RED + "Alarm!" + ANSI_RESET);
+                break;
+            }
+            System.out.println(arg + ": " + arg.length() + " letter(s)");
+        }
+    }
 }
