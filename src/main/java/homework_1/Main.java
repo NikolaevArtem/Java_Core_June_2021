6e9d1df74de5d0e2b7728833fd16b42b31f080f9
Alexander belyaevskov <alexandr.belyaevskov@itplatforma.ru>
2021-07-10 11:43:44 +0300

    simplifying solution

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -5,38 +5,22 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Current codepage: " + Charset.defaultCharset());
-    if (Charset.defaultCharset() != StandardCharsets.UTF_8) {
-      System.out.println("For correct display cyrillic args need to run with \"-Dfile.encoding=\"UTF-8\"\"");
+    public static void main(String[] args) {
+        if (Charset.defaultCharset() != StandardCharsets.UTF_8) {
+            System.out.println("For correct display cyrillic args need to run with \"-Dfile.encoding=\"UTF-8\"\"");
+        }
+        try {
+            if (args.length == 0) {
+                System.out.println("No arguments provided!");
+            } else for (String arg : args) {
+                if (arg.matches("error")) {
+                    throw new IOException("Alarm!");
+                }
+                System.out.printf("%s : %s letter(s)!\n", arg, arg.length());
+            }
+        } catch (IOException e) {
+            System.err.println("Error!");
+            e.printStackTrace();
+        }
     }
-    try {
-      if (args.length == 0) {
-        System.out.println("No arguments provided!");
-      } else printOutput(args);
-    } catch (IOException e) {
-      System.err.println("Error!");
-      e.printStackTrace();
-    }
-  }
-
-  private static void printOutput(String[] inputArgs) throws IOException {
-    for (String arg : inputArgs) {
-      if (arg.matches("error")) {
-        throw new IOException("Alarm!");
-      }
-
-      StringBuilder charBuffer = new StringBuilder();
-      charBuffer.append("Argument: ");
-//      charBuffer.append(arg.length());
-//      charBuffer.append(" letters");
-
-      String[] lettersInArg = arg.split("");
-      int outputLength = Math.min(lettersInArg.length, 8); //cut off symbols more then 8 elements
-      for (int i = 0; i < outputLength; i++) {
-        charBuffer.append(lettersInArg[i]);
-      }
-      System.out.println(charBuffer);
-    }
-  }
 }
