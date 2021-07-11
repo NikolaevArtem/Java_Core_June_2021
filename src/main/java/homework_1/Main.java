0c9b7109a024bebc4d69ee48e464e618a458d1a5
Kollega1984 <bomba_25@mail.ru>
2021-07-11 23:27:17 +0300

    changed README

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -4,18 +4,14 @@ public class Main {
 
     public static final String redColor = "\u001B[31m";
     public static final String closeColor = "\u001B[0m";
+
     public static void main(String[] args) {
-
-
             for (String str: args){
-
                     if(str.equals("ошибка")){
                         System.out.println(redColor+"Тревога!"+closeColor);
                         break;
                     }
                     System.out.println(str + ": " + str.length() + " букв");
-
-
             }
 
 
