71f59ad8dea7ffda39a9aae9f62c95fcf95f2bec
Zaytsev Artem <Zaytsev.A@spbec-mining.ru>
2021-07-11 16:34:47 +0300

    my first commit...again (after deleting my old branch, with the incorrect name,and creating the new one with the valid name...) + homework_1 + README.md

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -1,9 +1,23 @@
 package homework_1;
 
 public class Main {
-
   public static void main(String[] args) {
-    System.out.println("Hello homework!");
+    int k = args.length;
+    if (k==0) {
+      System.out.println("\033[32mApplication has been called \033[31mwithout\033[32m the arguments!!! \033[0m");
+    } else {
+      for (int i=0; i<=k-1;i++) {
+        if (args[i].equals("error"))//не забыть написать КРАСНЫМ цветом!!!
+        {
+          System.out.println("\033[31mAlarm! \033[0m");//https://stackoverflow.com/questions/565252/how-to-set-a-strings-color
+          break;//выходим
+        }
+        else {
+          String s = "letter";
+          if (args[i].length()>1) {s=s+"s";}
+          System.out.println(args[i]+": "+args[i].length()+" "+s);
+        }
+      }
+    }
   }
-
 }
