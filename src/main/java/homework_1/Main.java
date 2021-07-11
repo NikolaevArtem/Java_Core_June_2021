75c4a4d5a8a0c43d52ddec50d83884a076dfa91e
LinaFawn <tcipkoalina2000@mail.ru>
2021-07-11 21:24:36 +0300

    hm_1_afterMerge

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -1,9 +1,18 @@
 package homework_1;
 
 public class Main {
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_RESET = "\u001B[0m";
 
   public static void main(String[] args) {
-    System.out.println("Hello homework!");
-  }
 
+    for(String a: args) {
+      if (a.equals("error")) {
+        System.out.println(ANSI_RED + "Alarm!" + ANSI_RESET);
+        break;
+      }
+      System.out.println(a + ": " + a.length());
+    }
+
+  }
 }
