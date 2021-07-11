266c2b840bd6d6ded507a98362cf7e5bd8e73542
Allzza <allzza4279@gmail.com>
2021-07-11 18:01:01 +0300

    connecting on new pc

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -3,7 +3,15 @@ package homework_1;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello homework!");
-  }
+    for (String object : args) {
+      if (object.equals("ошибка")) {
+        System.out.println("Тревога");
+        break;
+      }
+      else {
+        System.out.println(object + ": " + object.length() + " букв");
+      }
+    }
 
+  }
 }
