aba2f92a8c55f09527c57292697579c6db32a1bb
OzoneTaTaY <volchenkovolk@mail.ru>
2021-07-11 23:19:39 +0300

    1. Added variable for arg.length() 2. Created method for ending addition 3. Fixed bad indention

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -1,45 +1,51 @@
 package homework_1;
 
 public class Main {
+    public static String StringMaker (String arg){
+        int length = arg.length();
+        StringBuilder result = new StringBuilder();
+        result
+                .append(arg)
+                .append(": ")
+                .append(arg.length());
+        switch (length % 10) {
+            case 1: {
+                if (length != 11) {
+                    result.append(" буква");
+                    break;
+                } else {
+                    result.append(" букв");
+                    break;
+                }
+            }
+            case 2:
+            case 3:
+            case 4: {
+                if (length != 12 && length != 13 && length != 14) {
+                    result.append(" буквы");
+                    break;
+                } else {
+                    result.append(" букв");
+                    break;
+                }
+            }
+            default: {
+                result.append(" букв");
+                break;
+            }
+        }
+        return result.toString();
+    }
+
     public static void main(String[] args) {
-        if (args.length == 0) return;
+        if (args.length == 0)
+            return;
         for (String arg : args) {
             if (arg.equals("ошибка")) {
                 System.err.println("Тревога!");
                 return;
             } else {
-                StringBuilder result = new StringBuilder();
-                result
-                        .append(arg)
-                        .append(": ")
-                        .append(arg.length());
-                switch (arg.length() % 10) {
-                    case 1: {
-                        if (arg.length() != 11) {
-                            result.append(" буква");
-                            break;
-                        } else {
-                            result.append(" букв");
-                            break;
-                        }
-                    }
-                    case 2:
-                    case 3:
-                    case 4: {
-                        if (arg.length() != 12 && arg.length() != 13 && arg.length() != 14) {
-                            result.append(" буквы");
-                            break;
-                        } else {
-                            result.append(" букв");
-                            break;
-                        }
-                    }
-                    default: {
-                        result.append(" букв");
-                        break;
-                    }
-                }
-                System.out.println(result);
+                System.out.println(StringMaker(arg));
             }
         }
     }
