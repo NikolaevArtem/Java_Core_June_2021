0421d59bcce2bf86b82c25cbee9895bfdc5ec759
bttrthnubtch <slowly@live.ru>
2021-07-12 00:17:42 +0300

    Refactoring

--- src/main/java/homework_1/CharCount.java
+++ src/main/java/homework_1/CharCount.java
@@ -2,18 +2,12 @@ package homework_1;
 
 public class CharCount {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("There is no parameters, exit program.");
-        } else {
-
-            System.out.println("Count of Char's in words:");
-            for (String arg : args) {
-                if (arg.equals("error")) {
-                    System.err.println("Alarm!");
-                    break;
-                }
-                System.out.println(arg + ": " + arg.length());
+        for (String arg : args) {
+            if (arg.equals("error")) {
+                System.err.println("Alarm!");
+                break;
             }
+            System.out.println(arg + ": " + arg.length() + " letters");
         }
     }
 }
