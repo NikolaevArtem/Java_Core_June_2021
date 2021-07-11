1feb7cdd5a7633aa11c9b617a83589c49c37f011
Kollega1984 <bomba_25@mail.ru>
2021-07-11 22:14:52 +0300

    fixed it

--- src/main/java/HomeWork_1/Work1.java
+++ src/main/java/HomeWork_1/Work1.java
@@ -1,14 +1,16 @@
 package HomeWork_1;
 
 public class Work1 {
+
+    public static final String redColor = "\u001B[31m";
+    public static final String closeColor = "\u001B[0m";
     public static void main(String[] args) {
 
-            if(args == null || args[0].isEmpty()) return;
 
             for (String str: args){
 
                     if(str.equals("ошибка")){
-                        System.out.println("\u001B[31m"+"Тревога!");
+                        System.out.println(redColor+"Тревога!"+closeColor);
                         break;
                     }
                     System.out.println(str + ": " + str.length() + " букв");
