13419a1f0930a0271fc74325f8ceba91712392ba
Konstantin Ivanov <kostya-ivanov9623@mail.ru>
2021-07-12 16:13:59 +0300

    first_homework

--- src/main/java/Main_one.java
+++ src/main/java/Main_one.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main_one {
 
   public static void main(String[] args) {
     System.out.println("Hello, World!");
--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -2,9 +2,11 @@ package homework_1;
 
 public class Main {
     public static void main(String[] args) {
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_RESET = "\u001B[0m";
         for (String arg : args) {
             if (arg.equals("Alarm")) {
-                System.err.println("Alarm");
+                System.out.println(ANSI_RED + "Alarm!" + ANSI_RESET);
                 break;
             } System.out.println(arg + ":" + arg.length());
         }
