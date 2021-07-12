789fe4f9783f5abeb9003a3309014b05d880ddc7
Artem Nikolaev <anikolaev@ebsco.com>
2021-07-13 01:23:59 +0500

    Revert changes in lesson_1

--- src/main/java/lesson_1/Main.java
+++ src/main/java/lesson_1/Main.java
@@ -3,15 +3,7 @@ package lesson_1;
 public class Main {
 
   public static void main(String[] args) {
-    Number n = 10;
-    Integer in = 10;
-    System.out.println(n == in);
-    short sh1 = 5;
-    short sh2 = 10;
-
-    short sh = (short) (sh2 - sh1);
-
-    System.out.println(sh);
+    System.out.println("Hello World!");
   }
 
 }
