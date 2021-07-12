aed8cb270b55707b888b9fbdd3b3c7f5170790d7
kirillfilipcik <kirill.filipchik@yandex.ru>
2021-07-12 08:41:56 +0300

    fix bugs moved the creation of result String -> new method createStringResult removed check for missing args

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -2,34 +2,36 @@ package homework_1;
 
 public class Main {
     public static void main(String... args) {
-        if (args.length == 0) return;
-        StringBuilder strBuilder = new StringBuilder();
         for (String arg :
                 args) {
-            if (arg.equalsIgnoreCase("ошибка")) {
+            if (arg. equals("ошибка")) {
                 System.err.println("Тревога!");
                 return;
             } else {
-                strBuilder
-                        .append(arg)
-                        .append(": ")
-                        .append(arg.length())
-                        .append(" букв");
-                switch (arg.length()%10) {
-                    case 1:
-                        strBuilder.append("а");
-                        break;
-                    case 2:
-                    case 3:
-                    case 4:
-                        strBuilder.append("ы");
-                        break;
-                    default:
-                        break;
-                }
-                System.out.println(strBuilder);
-                strBuilder.setLength(0);
+                System.out.println(createStringResult(arg));
             }
         }
+    }
+
+    private static String createStringResult(String str) {
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder
+                .append(str)
+                .append(": ")
+                .append(str.length())
+                .append(" букв");
+        switch (str.length()%10) {
+            case 1:
+                strBuilder.append("а");
+                break;
+            case 2:
+            case 3:
+            case 4:
+                strBuilder.append("ы");
+                break;
+            default:
+                break;
+        }
+        return strBuilder.toString();
     }
 }
