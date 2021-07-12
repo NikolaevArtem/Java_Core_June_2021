2fc8a831f5f1a9957e1dfe2114bfb6ad5b508038
bttrthnubtch <slowly@live.ru>
2021-07-12 16:23:24 +0300

    Final refactoring

--- src/main/java/homework_1/CharCount.java
+++ src/main/java/homework_1/CharCount.java
@@ -1,10 +1,13 @@
 package homework_1;
 
 public class CharCount {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
         for (String arg : args) {
             if (arg.equals("error")) {
-                System.err.println("Alarm!");
+                System.out.println(ANSI_RED + "Alarm!" + ANSI_RESET);
                 break;
             } else {
                 System.out.println(arg + ": " + arg.length() + " letters");
