366079a2f5d83c6b96f166920e613f0f6188ca65
Konstantin Ivanov <kostya-ivanov9623@mail.ru>
2021-07-12 14:56:22 +0300

    first_homework

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -3,10 +3,10 @@ package homework_1;
 public class Main {
     public static void main(String[] args) {
         for (String arg : args) {
-            if (arg.equals("ошибка")) {
-                System.err.println("ошибка");
+            if (arg.equals("Alarm")) {
+                System.err.println("Alarm");
                 break;
-            } System.out.println(arg + ":" + arg.length() + " букв");
+            } System.out.println(arg + ":" + arg.length());
         }
     }
 }
