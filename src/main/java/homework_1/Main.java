0939a720e653e4984aff5f13fec91be33a76c790
Drozdov <Drozdov@lanit.ru>
2021-07-12 13:36:30 +0300

    del serr, add "\033[0;31m" in sout

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         for (String arg : args) {
             if (arg.equals("ошибка")) {
-                System.err.println("Тревога!");
+                System.out.println("\033[0;31m" + "Тревога!" + "\033[0m");
                 break;
             }
             System.out.println(arg + ": " + arg.length() + " букв");
