ce8002fb34582607e879b58944b9286fa553a622
kirillfilipcik <kirill.filipchik@yandex.ru>
2021-07-12 08:41:56 +0300

    fix bugs add color -> instead -> System.err the last commit deleted the main, returned it.

---
+++ src/main/java/homework_1/Main.java
@@ -0,0 +1,39 @@
+package homework_1;
+
+public class Main {
+  public static final String redColorOutString = "\u001B[31m";
+  public static final String closeColorOutString = "\u001B[0m";
+    public static void main(String... args) {
+        for (String arg :
+                args) {
+            if (arg. equals("ошибка")) {
+                System.out.println(redColorOutString + "Тревога!" + redColorOutString);
+                return;
+            } else {
+                System.out.println(createStringResult(arg));
+            }
+        }
+    }
+
+    private static String createStringResult(String str) {
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder
+                .append(str)
+                .append(": ")
+                .append(str.length())
+                .append(" букв");
+        switch (str.length()%10) {
+            case 1:
+                strBuilder.append("а");
+                break;
+            case 2:
+            case 3:
+            case 4:
+                strBuilder.append("ы");
+                break;
+            default:
+                break;
+        }
+        return strBuilder.toString();
+    }
+}
