1a26679040dbf911888fdeb6809f4257d1b76640
Alexander belyaevskov <alexandr.belyaevskov@itplatforma.ru>
2021-07-12 12:03:26 +0300

    removed raising exeption, fixed output

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -9,18 +9,14 @@ public class Main {
         if (Charset.defaultCharset() != StandardCharsets.UTF_8) {
             System.out.println("For correct display cyrillic args need to run with \"-Dfile.encoding=\"UTF-8\"\"");
         }
-        try {
-            if (args.length == 0) {
-                System.out.println("No arguments provided!");
-            } else for (String arg : args) {
-                if (arg.matches("error")) {
-                    throw new IOException("Alarm!");
-                }
-                System.out.printf("%s : %s letter(s)\n", arg, arg.length());
+        if (args.length == 0) {
+            System.out.println("No arguments provided!");
+        } else for (String arg : args) {
+            if (arg.matches("error")) {
+                System.err.println("Alarm!");
+                break;
             }
-        } catch (IOException e) {
-            System.err.println("Error!");
-            e.printStackTrace();
+            System.out.printf("%s: %s letter(s)\n", arg, arg.length());
         }
     }
 }
