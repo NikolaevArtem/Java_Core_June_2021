766bfbed58bbfc3138f7b3ffb919c3937946a218
Dmitriy Vaschenko <dvaschenko33@gmail.com>
2021-07-12 19:59:36 +0300

    Correct solution

--- src/main/java/homework/task1/Task1.java
+++ src/main/java/homework/task1/Task1.java
@@ -8,30 +8,13 @@ public class Task1 {
 
                 String toPrint = args[i];
                 if (toPrint.matches("error")){                                //checking for error
-                    System.err.println("Alarm!");
+                    System.out.println("____Alarm!____");
                     System.exit(0);
                 }
 
-                toPrint = checkString(toPrint);
-                System.out.println("Argument:" + toPrint);
+                System.out.println("Argument: " + toPrint +", "+ toPrint.length() + " letter(s)");
             }
         }
         else System.exit(0);
-    }
-
-    public static String checkString(String s){                                   // checking String length, return 8 symbol string
-
-        int sLength = s.length();
-        return switch (sLength) {
-            case 8 -> s;
-            case 7 -> s + "_";
-            case 6 -> s + "__";
-            case 5 -> s + "___";
-            case 4 -> s + "____";
-            case 3 -> s + "_____";
-            case 2 -> s + "______";
-            case 1 -> s + "_______";
-            default -> s.substring(0, 7);
-        };
     }
 }
