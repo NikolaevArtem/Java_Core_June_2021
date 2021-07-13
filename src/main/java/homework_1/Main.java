9d17d8436145ef79bd8baafd37d8a4e77b69f08c
semenantufiev <alexantufiev@gmail.com>
2021-07-13 20:19:51 +0300

    Fixed style

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -3,14 +3,13 @@ package homework_1;
 public class Main {
 
   public static void main(String[] args) {
-
     for (String arg : args) {
       if (arg.equals("error")) {
         System.out.println("\u001B[41m" + "Alarm!" + "\u001B[0m");
         break;
       }
-      System.out.println(arg + ": " + arg.length() + "letters");
+
+      System.out.println(arg + " : " + arg.length() + " letters");
     }
   }
-
 }
