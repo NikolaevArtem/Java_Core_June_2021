5bb35c283433ae1f482d9aefab9a5acf77826103
Konstantin Ivanov <kostya-ivanov9623@mail.ru>
2021-07-13 22:46:37 +0300

    delete Main

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -1,16 +1,19 @@
 package homework_1;
 
 public class Main {
-    private static final String RED = "\u001B[31m";
-    private static final String RESET = "\u001B[0m";
+
+    public static final String ANSI_RED = "\u001B[31m";
+
+    public static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
-        for(String argument: args){
-            if(argument.equals("ошибка")){
-                System.out.println(RED + "Тревога!" + RESET);
+        for (String s : args) {
+            if (s.equals("error")) {
+                System.out.println(ANSI_RED + "Alarm!" + ANSI_RESET);
                 break;
+            } else {
+                System.out.println(s + ": " + s.length() + " letters");
             }
-            System.out.println(argument + ": " + argument.length() + " букв");
         }
     }
 }
