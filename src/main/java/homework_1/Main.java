d487f3221063bd2baa19b7e7aa6fc36070f77a3c
tatianadzn <tatiana22d@gmail.com>
2021-07-13 05:43:26 +0300

    different streams output fixed

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -2,11 +2,14 @@ package homework_1;
 
 public class Main {
     public static void main(String[] args) {
+        final String RED_FONT = "\033[0;31m";
+        final String RESET_FONT = "\033[0m";
+
         for (String arg : args) {
             if (!arg.equals("error")){
                 System.out.println(arg + ": " + arg.length() + " letters");
             } else{
-                System.err.println("Alarm!");
+                System.out.println(RED_FONT + "Alarm!" + RESET_FONT);
                 break;
             }
         }
