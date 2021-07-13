ad6354cab8b061cfc186c82dc2e12568712cb582
Konstantin Ivanov <kostya-ivanov9623@mail.ru>
2021-07-13 17:00:53 +0300

    second_homework

---
+++ src/main/java/homework_2/TrafficLight.java
@@ -0,0 +1,40 @@
+package homework_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TrafficLight {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        while (count < 3) {
+            int light = Integer.parseInt(reader.readLine());
+            int res = 60;
+            int result = light % res;
+            if (light < 0) {
+                System.out.println("Please use positive number");
+                count++;
+            }
+            if (light > 86399) {
+                System.out.println("Please use number between 0 and 86399");
+                count++;
+            } else {
+                if (result >= 0 && result < 35) {
+                    System.out.println(ANSI_GREEN + "green" + ANSI_RESET);
+                }
+                if (result >= 35 && result < 40) {
+                    System.out.println(ANSI_YELLOW + "yellow" + ANSI_RESET);
+                }
+                if (result >= 40 && result < 60) {
+                    System.out.println(ANSI_RED + "red" + ANSI_RESET);
+                }
+            }
+        }
+    }
+}
