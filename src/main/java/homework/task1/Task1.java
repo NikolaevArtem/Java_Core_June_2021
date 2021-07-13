541303660c4a4dd10bb89833f65791328c89cb9b
Dmitriy Vaschenko <dvaschenko33@gmail.com>
2021-07-13 22:52:09 +0300

    Red added, System.exit removed.

--- src/main/java/homework/task1/Task1.java
+++ src/main/java/homework/task1/Task1.java
@@ -8,13 +8,19 @@ public class Task1 {
 
                 String toPrint = args[i];
                 if (toPrint.matches("error")){                                //checking for error
-                    System.out.println("____Alarm!____");
-                    System.exit(0);
+                    System.out.println(ConsoleCollors.RED + "Alarm!" + ConsoleCollors.RESET);
+                    break;
                 }
 
                 System.out.println("Argument: " + toPrint +", "+ toPrint.length() + " letter(s)");
             }
         }
-        else System.exit(0);
+    }
+    public static class ConsoleCollors{
+        // Reset
+        public static final String RESET = "\033[0m";  // Text Reset
+
+        // Red
+        public static final String RED = "\033[0;31m";     // RED
     }
 }
