709d3d3281a1297b0c78ddbdf6436a54319588c5
Kollega1984 <bomba_25@mail.ru>
2021-07-14 22:58:31 +0300

    fix bugs

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -1,18 +1,27 @@
-package HomeWork_1;
+package homework_1;
+
+/*
+ * Main
+ *
+ * v2
+ *
+ * Prihodko Dmitriy
+ */
+
 
 public class Main {
 
-    public static final String redColor = "\u001B[31m";
-    public static final String closeColor = "\u001B[0m";
+    public static final String RED_COLOR = "\u001B[31m";
+    public static final String CLOSE_COLOR = "\u001B[0m";
 
     public static void main(String[] args) {
-            for (String str: args){
-                    if(str.equals("ошибка")){
-                        System.out.println(redColor+"Тревога!"+closeColor);
-                        break;
-                    }
-                    System.out.println(str + ": " + str.length() + " букв");
+        for (String str : args) {
+            if (str.equals("ошибка")) {
+                System.out.println(RED_COLOR + "Тревога!" + CLOSE_COLOR);
+                break;
             }
+            System.out.println(str + ": " + str.length() + " букв");
+        }
 
 
     }
