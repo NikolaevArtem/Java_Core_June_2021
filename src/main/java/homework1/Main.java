e8dd4be4edbcc27343f9c86be065f43ba5af429b
Daisy <la.reine.m@gmai.com>
2021-07-14 04:08:17 +0300

    Homework 1 corrected

--- src/main/java/homework1/Main.java
+++ src/main/java/homework1/Main.java
@@ -1,13 +1,24 @@
 package homework1;
 
 public class Main {
+    static String word;
 
-    public static String wordAndNumber (String word){
+    public static String wordAndNumberOfLetters(String word) {
+
 
         if (word.equals("ошибка")) {
-           return "\u001b[31m" + " ТРЕВОГА";
-        } else return word + ": " + word.length();
+            return "\u001b[31m" + " ТРЕВОГА";
 
+        } else if (!word.equals("ошибка")) {
+            return word + ": " + word.length() + " letters.";
+
+            //added closing String
+        } else return "";
+    }
+
+    // added psvm
+    public static void main(String[] args) {
+        System.out.println(wordAndNumberOfLetters(word));
     }
 
 }
