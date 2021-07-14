b7b3c70fcb16d3db3331bcd00a00f1f749fc016e
tatianadzn <tatiana22d@gmail.com>
2021-07-14 13:00:46 +0300

    style hm1 fixed

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -1,16 +1,17 @@
 package homework_1;
 
 public class Main {
+
     public static void main(String[] args) {
         final String RED_FONT = "\033[0;31m";
         final String RESET_FONT = "\033[0m";
 
         for (String arg : args) {
-            if (!arg.equals("error")){
-                System.out.println(arg + ": " + arg.length() + " letters");
-            } else{
+            if (arg.equals("error")){
                 System.out.println(RED_FONT + "Alarm!" + RESET_FONT);
                 break;
+            } else {
+                System.out.println(arg + ": " + arg.length() + " letters");
             }
         }
     }
