37be3726a488dd4ef9fd3f170affcce60bbc48ed
Kollega1984 <bomba_25@mail.ru>
2021-07-14 23:49:07 +0300

    Traffic_light v2

---
+++ src/main/java/homework_2/Traffic_light.java
@@ -0,0 +1,52 @@
+package homework_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * Traffic_light
+ *
+ * v1
+ *
+ * Prihodko Dmitriy
+ */
+
+public class Traffic_light {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int seconds = 0;
+        try {
+            seconds = Integer.parseInt(reader.readLine());
+
+            if(seconds>86399)
+                throw new IndexOutOfBoundsException();
+            if(seconds<0)
+                throw new ArithmeticException();
+        } catch (NumberFormatException e) {
+            System.out.println("ошибка, только числа");
+            return;
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ошибка, недопустимое значение");
+            return;
+        } catch (ArithmeticException e) {
+            System.out.println("ошибка, только позитивные");
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(seconds>60)
+            seconds %= 60;
+
+        if (seconds<35){
+            System.out.println("зеленый");
+        } else if(seconds<40){
+            System.out.println("желтый");
+        } else if(seconds<55){
+            System.out.println("красный");
+        } else {
+            System.out.println("желтый");
+        }
+    }
+}
