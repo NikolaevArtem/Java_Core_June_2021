8b478c0ee897dead176c4b26864ef84223d9201e
Dmitrii Troshkin <86798105+Allzza@users.noreply.github.com>
2021-07-14 21:09:49 +0300

    Update src/main/java/homework_1/Main.java
    Co-authored-by: Artem Nikolaev <Nikolaev.artem.work@yandex.ru>

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -3,13 +3,12 @@ package homework_1;
 public class Main {
 
   public static void main(String[] args) {
-    for (String object : args) {
-      if (object.equals("ошибка")) {
+    for (String s: args) {
+      if (s.equals("ошибка")) {
         System.out.println("Тревога");
         break;
-      }
-      else {
-        System.out.println(object + ": " + object.length() + " букв");
+      } else {
+        System.out.println(s + ": " + s.length() + " букв");
       }
     }
 
