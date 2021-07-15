e63862fff90b9a0968af1de15ba6a45cfa0b6603
Anton Miazin <revan-00001@yandex.ru>
2021-07-15 22:38:23 +0300

    HW1 renaming & optional fixes

--- src/main/java/Homework_1/ConsolePrinter.java
+++ src/main/java/Homework_1/ConsolePrinter.java
@@ -1,14 +1,16 @@
 package Homework_1;
 
-public class Main {
+public class ConsolePrinter {
     public static void main(String[] args) {
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_RESET = "\u001B[0m\n";
         for (String arg : args) {
             if (arg.equals("error")) {
-                System.out.println("\u001B[31m" + "Alarm!" + "\u001B[0m\n");
+                System.out.println(ANSI_RED + "Alarm!" + ANSI_RESET);
                 break;
             } else {
                 System.out.println(arg + " : " + arg.length() + " letters");
             }
         }
-            }
-        }
+    }
+}
