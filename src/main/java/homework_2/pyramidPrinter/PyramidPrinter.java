6bba87887b73e611d3baa8401f42b9188c6fc717
arkadiy krylosov <arkady.krylosov@yandex.ru>
2021-07-15 17:06:12 +0300

    homework_2 done.

--- src/main/java/homework_2/pyramidPrinter/PyramidPrinter.java
+++ src/main/java/homework_2/pyramidPrinter/PyramidPrinter.java
@@ -8,13 +8,13 @@ public class PyramidPrinter {
     public static void main(String[] args) {
         String output = "";
         int input;
-        try(BufferedReader bF = new BufferedReader(new InputStreamReader(System.in))){
+        try (BufferedReader bF = new BufferedReader(new InputStreamReader(System.in))) {
             input = Integer.parseInt(bF.readLine());
             for (int i = 0; i < input; i++) {
                 output += "x";
                 System.out.println(output);
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error occurred during input");
         }
     }
