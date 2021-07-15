88817ca497d4369846e8fc7123849055ed192b56
ijustwannatry <dmitrisedam11@gmail.com>
2021-07-15 12:07:58 +0300

    HomeWork_2 Traffic light - Done. ver_2.2 Pyramid Printer - Done. Ver_1.1 Random Chars Table - Done. Ver_1.0 README fixed.

--- src/main/java/homework_2/PyramidPrinter.java
+++ src/main/java/homework_2/PyramidPrinter.java
@@ -8,9 +8,13 @@ public class PyramidPrinter {
     public static void main(String[] args) {
         System.out.println("How many levels should the pyramid be?");
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            int levels = Integer.parseInt(reader.readLine());
-            for (int i = 1; i <= levels; i++) {
-                System.out.println("x".repeat(i));
+            try {
+                int levels = Integer.parseInt(reader.readLine());
+                for (int i = 1; i <= levels; i++) {
+                    System.out.println("x".repeat(i));
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("It's not a number. Next time enter a positive integer.");
             }
         } catch (IOException e) {
             System.out.println("Error");
