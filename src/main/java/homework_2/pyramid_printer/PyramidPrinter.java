3a09f3f9dc86a35b1f9452e2d5cffae184ed80ef
Waranko <mari.waranko@gmail.com>
2021-07-15 23:33:47 +0300

    Pyramid Printer done

--- src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
+++ src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
@@ -1,2 +1,19 @@
-package homework_2.pyramid_printer;public class PyramidPrinter {
+package homework_2.pyramid_printer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PyramidPrinter {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int pyramidSize = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= pyramidSize; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("x ");
+            }
+            System.out.println();
+        }
+    }
 }
