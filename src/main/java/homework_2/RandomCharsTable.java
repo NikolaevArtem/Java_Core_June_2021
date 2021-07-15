db0203ea8a13d79e6847b2613989ca8ba5118927
ijustwannatry <dmitrisedam11@gmail.com>
2021-07-15 12:36:40 +0300

    HomeWork_2 Traffic light - Done. ver_2.2 Pyramid Printer - Done. Ver_1.1 Random Chars Table - Done. Ver_1.0 README fixed.

--- src/main/java/homework_2/RandomCharsTable.java
+++ src/main/java/homework_2/RandomCharsTable.java
@@ -11,27 +11,22 @@ public class RandomCharsTable {
                 " and the width of a table and a strategy(even/odd) ");
         System.out.println("Input example: 2 2 even\n");
         System.out.print("Enter your data: ");
-
         try (Scanner scn = new Scanner(System.in)) {
             int length = scn.nextInt();
             int width = scn.nextInt();
             int remainder = "odd".equals(scn.next()) ? 1 : 0;
             StringBuilder message = new StringBuilder(remainder == 0 ?
                     "Even letters -" : "Odd letters -" ) ;
-
             char[][] table = new char[length][width];
             Random rnd = new Random(System.currentTimeMillis());
             for (int i = 0; i < length; i++) {
                 System.out.print("| ");
                 for (int j = 0; j < width; j++) {
-
                     table[i][j] = (char) (rnd.nextInt(26) + 65);
                     System.out.print(table[i][j] + " | ");
-
                     if (table[i][j] % 2 == remainder) {
                         message.append(" ").append(table[i][j]).append(",");
                     }
-
                 }
                 System.out.println();
             }
