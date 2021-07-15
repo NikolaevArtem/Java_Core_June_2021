0e704daec45e53aae745e6cefa1bcddeb7f74286
arkadiy krylosov <arkady.krylosov@yandex.ru>
2021-07-15 17:28:39 +0300

    homework_2 done.

--- src/main/java/homework_2/trafficLight/TrafficLight.java
+++ src/main/java/homework_2/trafficLight/TrafficLight.java
@@ -1,4 +1,10 @@
 package homework_2.trafficLight;
 
+import java.io.IOException;
+
 public class TrafficLight {
+    public static void main(String[] args) throws IOException {
+        Strategy strategy = new Strategy();
+        strategy.choosingStrategy();
+    }
 }
