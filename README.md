cebd3ffc3f48e00613bf80f752e848831de34235
OzoneTaTaY <volchenkovolk@mail.ru>
2021-07-16 21:25:10 +0300

    HW_2 Traffic Light task complete

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 | Number | Solution  | Short description
 | --- | --- | --- |
 | HW1 | [Console printer](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/SergeyVolchenkov/src/main/java/homework_1) | The app that reads input arguments and prints them, until "error" argument |
+| HW2 | [Traffic Light](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/SergeyVolchenkov/src/main/java/homework_2/TrafficLight) | The app reads number of seconds since the beginning of the day from the command line and prints the colour of traffic light |
---
+++ src/main/java/homework_2/TrafficLight.java
@@ -0,0 +1,39 @@
+package homework_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TrafficLight {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.println("Enter seconds or \"stop\"");
+                String inputStr = bufferedReader.readLine();
+                try {
+                    if (inputStr.equals("stop")) {
+                        System.out.println("Exit...");
+                        break;
+                    }
+                    int seconds = Integer.parseInt(inputStr);
+                    if (seconds < 0 || seconds > 86399) {
+                        System.err.println("Input String is out of range");
+                    } else if (seconds % 60 < 35) {
+                        System.out.println("Colour of traffic light is GREEN");
+                    } else if (seconds % 60 < 40) {
+                        System.out.println("Colour of traffic light is YELLOW");
+                    } else if (seconds % 60 < 55) {
+                        System.out.println("Colour of traffic light is RED");
+                    } else {
+                        System.out.println("Colour of traffic light is YELLOW");
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("Can't parse input string to integer");
+                }
+            } catch (IOException e) {
+                System.err.println("Buffered reader throws IOException");
+            }
+        }
+    }
+}
