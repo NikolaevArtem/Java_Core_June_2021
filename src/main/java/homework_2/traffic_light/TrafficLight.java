bdf3eff886a5708f029e3adcea601f6218af75d6
Waranko <mari.waranko@gmail.com>
2021-07-16 03:19:33 +0300

    Traffic Light add mode

--- src/main/java/homework_2/traffic_light/TrafficLight.java
+++ src/main/java/homework_2/traffic_light/TrafficLight.java
@@ -7,14 +7,37 @@ import java.io.InputStreamReader;
 public class TrafficLight {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Choose input mode:\n1 - number of seconds\n2 - hh:mm:ss");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String answer = br.readLine();
+
         int seconds;
-        try {
-            seconds = Integer.parseInt(br.readLine());
-        } catch (NumberFormatException nfe) {
-            System.out.println("Input error: The number of seconds can be a number only!");
+        if(answer.equals("1")){
+            try {
+                seconds = Integer.parseInt(br.readLine());
+            } catch (NumberFormatException nfe) {
+                System.out.println("Input error: The number of seconds can be a number only!");
+                return;
+            }
+        } else if(answer.equals("2")){
+            String[] time = br.readLine().split(":");
+            if(time.length != 3){
+                System.out.println("Input error: Wrong input format!");
+                return;
+            }
+            try {
+                int hours = Integer.parseInt(time[0]);
+                int minutes = Integer.parseInt(time[1]);
+                seconds = 60 * 60 * hours + 60 * minutes + Integer.parseInt(time[2]);
+            } catch (NumberFormatException nfe) {
+                System.out.println("Input error: Values can be numbers only!");
+                return;
+            }
+        } else{
+            System.out.println("Unrecognizable answer!");
             return;
         }
+
         if (seconds < 0) {
             System.out.println("Input error: The number of seconds should be positive!");
             return;
