6216411b30c2372e74ffbc78553f7ec3062d33ee
Daisy <la.reine.m@gmai.com>
2021-07-17 12:31:44 +0300

    Added closing color string

--- src/main/java/Homework1/Main.java
+++ src/main/java/Homework1/Main.java
@@ -1,2 +1,19 @@
-package Homework1;public class Main {
+package Homework1;
+
+public class Main {
+    public static void main(String[] args) {
+        final String RED_COLOR = "\u001b[31m";
+        final String CLOSE_COLOR = "\u001B[0m";
+
+        for (String s : args) {
+            if (s.equals("error")) {
+                System.out.println(RED_COLOR + "ALARM" + CLOSE_COLOR);
+                break;
+
+            } else {
+                System.out.println(s + ": " + s.length() + " letters.");
+            }
+        }
+    }
 }
+
