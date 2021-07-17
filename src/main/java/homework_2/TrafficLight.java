e41d953cd94248b48dd697723e64fa1fd99d5b81
Sotheres <sotheres@gmail.com>
2021-07-17 15:20:30 +0300

    LocalTime version

--- src/main/java/homework_2/TrafficLight.java
+++ src/main/java/homework_2/TrafficLight.java
@@ -1,5 +1,7 @@
 package homework_2;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -57,14 +59,10 @@ public class TrafficLight {
             System.out.print("Enter time in hh:mm:ss format: ");
             try {
                 String line = scan.next();
-                String[] input = line.split(":");
-                int hh = Integer.parseInt(input[0]);
-                int mm = Integer.parseInt(input[1]);
-                int ss = Integer.parseInt(input[2]);
-                validateInputMode2(hh, mm, ss, input.length);
-                seconds = ss + mm * 60 + hh * 3600;
-            } catch (NumberFormatException e) {
-                throw new NumberFormatException("Not a hh:mm:ss format");
+                LocalTime lc = LocalTime.parse(line);
+                seconds = lc.toSecondOfDay();
+            } catch (DateTimeParseException e) {
+                throw new DateTimeParseException("Not a hh:mm:ss format", e.getParsedString(), e.getErrorIndex());
             }
         }
     }
@@ -75,21 +73,6 @@ public class TrafficLight {
         }
         if (i >= 86400) {
             throw new IllegalArgumentException("Day is over");
-        }
-    }
-
-    private static void validateInputMode2(int hh, int mm, int ss, int length) {
-        if (hh < 0 || hh > 23) {
-            throw new IllegalArgumentException("Hours is invalid");
-        }
-        if (mm < 0 || mm > 59) {
-            throw new IllegalArgumentException("Minutes is invalid");
-        }
-        if (ss < 0 || ss > 59) {
-            throw new IllegalArgumentException("Seconds is invalid");
-        }
-        if (length > 3) {
-            throw new IllegalArgumentException("Not a hh:mm:ss format");
         }
     }
 }
