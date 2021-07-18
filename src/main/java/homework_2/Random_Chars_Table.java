4565ca0d2f9d33dc92a96b568f891313e41c1cf9
Kollega1984 <bomba_25@mail.ru>
2021-07-19 01:18:31 +0300

    Random_Chars_Table v1

--- src/main/java/homework_2/Random_Chars_Table.java
+++ src/main/java/homework_2/Random_Chars_Table.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Random_Chars_Table {
     public static void main(String[] args) throws IOException {
@@ -13,42 +13,57 @@ public class Random_Chars_Table {
         int x = Integer.parseInt(reader.readLine());
         int y = Integer.parseInt(reader.readLine());
         String word = reader.readLine();
+
         char[][] table = new char[x][y];
         int helper;
-        ArrayList<Character> resultEven = new ArrayList<>();
-        ArrayList<Character> resultOdd = new ArrayList<>();
+        ArrayList<Integer> words = new ArrayList<>();
 
-        for(int i=0; i<x;i++){
-            for (int j=0; j<y; j++){
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
                 helper = getRandomNumber();
                 table[i][j] = (char) helper;
-                if(helper%2==0){
-                    resultEven.add((char) helper);
-                } else {
-                    resultOdd.add((char) helper);
-                }
+                words.add(helper);
 
             }
         }
-        for(int i=0; i<x;i++){
-            for (int j=0; j<y; j++){
-                System.out.print("|"+table[i][j]);
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print("|" + table[i][j]);
             }
             System.out.print("|");
             System.out.println();
         }
-        if(word.equals("even")){
-            for(Character i: resultEven){
-                System.out.print(i+",");
-            }
-        } else if(word.equals("odd")){
-            for(Character i: resultOdd){
-                System.out.print(((char)i)+",");
-            }
+
+        if (word.equals("even")) {
+
+            System.out.println(getResultChar(words, true));
+
+        } else if (word.equals("odd")) {
+
+            System.out.println(getResultChar(words, false));
+
         }
 
     }
+
     public static int getRandomNumber() {
         return (int) ((Math.random() * (90 - 65)) + 65);
+    }
+
+    public static String getResultChar(ArrayList<Integer> words, boolean even) {
+        String res;
+        if (even) {
+            res = words.stream().filter(i -> i % 2 == 0)
+                                .distinct()
+                                .map(i -> String.valueOf((char) i.intValue()))
+                                .collect(Collectors.joining(","));
+        } else {
+            res = words.stream().filter(i -> i % 2 == 1)
+                                .distinct()
+                                .map(i -> String.valueOf((char) i.intValue()))
+                                .collect(Collectors.joining(","));
+        }
+        return res;
     }
 }
