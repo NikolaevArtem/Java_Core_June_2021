b21edbb8ad0f4911b848fe5a6ea398b44dd34ba7
kirillfilipcik <kirill.filipchik@yandex.ru>
2021-07-19 06:40:54 +0300

    homework 2 ready for check. remade enum Color and as a consequence class TrafficLight

--- src/main/java/homework_2/traffic/Color.java
+++ src/main/java/homework_2/traffic/Color.java
@@ -1,7 +1,28 @@
 package homework_2.traffic;
 
 public enum Color {
-    GREEN,
+    GREEN(0, 35),
     YELLOW,
-    RED
+    RED(40, 55);
+
+    private int start;
+    private int end;
+
+    Color() {
+    }
+
+    Color(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public static Color check(int sec) {
+        if (sec >= GREEN.start && sec < GREEN.end) {
+            return GREEN;
+        } else if (sec >= RED.start && sec < RED.end) {
+            return RED;
+        } else {
+            return YELLOW;
+        }
+    }
 }
--- src/main/java/homework_2/traffic/TrafficLight.java
+++ src/main/java/homework_2/traffic/TrafficLight.java
@@ -10,10 +10,6 @@ public class TrafficLight extends AppWithIntInput {
     protected final static String ERR_MAX_MSG = "Error: день закончен)";
 
     private final static int MAX_VALUE = 86399;
-    private final static int GREEN_START = 0;
-    private final static int GREEN_END = 35;
-    private final static int RED_START = 40;
-    private final static int RED_END = 55;
 
     public static void main(String[] args) {
         Executable trafficLight = new TrafficLight();
@@ -26,19 +22,11 @@ public class TrafficLight extends AppWithIntInput {
         if(error != null) {
             return error;
         }
-        Color color;
         if (data > MAX_VALUE) {
             return (ERR_MAX_MSG);
         } else {
             int sec = data % 60;
-            if (sec >= GREEN_START && sec < GREEN_END) {
-                color = Color.GREEN;
-            } else if (sec >= RED_START && sec < RED_END) {
-                color = Color.RED;
-            } else {
-                color = Color.YELLOW;
-            }
-            return color.name();
+            return Color.check(sec).name();
         }
     }
 }
