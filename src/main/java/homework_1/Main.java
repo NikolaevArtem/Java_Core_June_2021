67f119c9aadcf7a17b089d4eb957612913a178d2
Allzza <allzza4279@gmail.com>
2021-07-19 18:16:36 +0300

    changed functionality according to description

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
   public static void main(String[] args) {
     for (String s: args) {
+
+        System.out.println(s + ": " + s.length() + " букв");
       if (s.equals("ошибка")) {
         System.out.println("Тревога");
         break;
-      } else {
-        System.out.println(s + ": " + s.length() + " букв");
-      }
+        }
     }
 
   }
