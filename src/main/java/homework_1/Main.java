4fe6027b9b68ba8756b40401f9603498e7ea2705
Dmitrii Troshkin <86798105+Allzza@users.noreply.github.com>
2021-07-19 19:15:53 +0300

    Update Main.java

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     for (String s: args) {
-
-        System.out.println(s + ": " + s.length() + " букв");
+      System.out.println(s + ": " + s.length() + " букв");
       if (s.equals("ошибка")) {
         System.out.println("Тревога");
         break;
