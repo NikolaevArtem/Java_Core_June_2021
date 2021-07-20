4ad9d12042fae47035d55dfaba1da33fc87ca072
kirillfilipcik <kirill.filipchik@yandex.ru>
2021-07-20 13:49:03 +0300

    homework 2 refactor TrafficLight

--- src/main/java/homework_2/traffic_light/TrafficLight.java
+++ src/main/java/homework_2/traffic_light/TrafficLight.java
@@ -4,13 +4,13 @@ import homework_2.utils.AppWithIntInput;
 
 public class TrafficLight extends AppWithIntInput {
 
-    protected final static String ERR_MAX_MSG = "Error: день закончен)";
+    protected final static String MAX_MSG = "The day is over";
     private final static int MAX_VALUE = 86399;
 
     @Override
     protected String calculate() {
         if (error != null) return error;
-        if (data > MAX_VALUE) return (ERR_MAX_MSG);
+        if (data > MAX_VALUE) return (MAX_MSG);
 
         int sec = data % 60;
         return Color.check(sec).name();
