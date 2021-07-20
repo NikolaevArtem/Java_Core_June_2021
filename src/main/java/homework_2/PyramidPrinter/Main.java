5e29806ccd4b02a21c66b3efc29661d2b0cc2be9
Alexandr Kirshin <kirhin1@gmail.com>
2021-07-20 15:08:28 +0300

    PyramidPrinter

--- src/main/java/homework_2/PyramidPrinter/Main.java
+++ src/main/java/homework_2/PyramidPrinter/Main.java
@@ -1,4 +1,8 @@
 package homework_2.PyramidPrinter;
 
 public class Main {
+
+    public static void main(String[] args) {
+        new PyramidPrinter().start();
+    }
 }
--- src/main/java/homework_2/PyramidPrinter/PyramidPrinter.java
+++ src/main/java/homework_2/PyramidPrinter/PyramidPrinter.java
@@ -6,16 +6,35 @@ import java.io.InputStreamReader;
 
 public class PyramidPrinter {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Pyramid Printer App \nEnter height: ");
+    private void errorMsg() {
+        System.out.println("Only 1 non-negative integer is allowed as passed parameter");
+    }
+
+    private void input() {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
             int height = Integer.parseInt(bufferedReader.readLine());
-            for (int i = 1; i <= height; i++) {
-                for (int j = 0; j < i; j++) {
-                    System.out.print("x");
-                }
-                System.out.println();
+            if (height < 1) {
+                throw new IllegalArgumentException();
             }
+            printerPyramid(height);
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+        } catch (IllegalArgumentException e) {
+            errorMsg();
         }
+    }
+
+    private void printerPyramid(int height) {
+        for (int i = 1; i <= height; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("x");
+            }
+            System.out.println();
+        }
+    }
+
+    public void start() {
+        System.out.println("Pyramid Printer App \nEnter height: ");
+        input();
     }
 }
