ba88fc3dc677310148a78e6fb24c38d97bb87a04
kirillfilipcik <kirill.filipchik@yandex.ru>
2021-07-22 02:22:07 +0300

    homework_3 ready for check add some tests.

--- src/test/java/homework_2/random_chars_table/RandomCharsTableTest.java
+++ src/test/java/homework_2/random_chars_table/RandomCharsTableTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RandomCharsTableTest extends UnitBase {
     @Test
@@ -25,6 +24,14 @@ class RandomCharsTableTest extends UnitBase {
         assertThrows(NoSuchElementException.class, () -> new RandomCharsTable().run());
     }
 
+    /*
+        for this test use new RandomCharsTable().setDebug(true).run();
+        3 3 odd
+        | K | N | W |
+        | N | E | E |
+        | J | G | D |
+        Even letters - N, N, J, D
+     */
     @Test
     void InputValidEvenArguments() {
         setInput("3 3 even");
@@ -39,13 +46,13 @@ class RandomCharsTableTest extends UnitBase {
     }
 
     /*
-    for this test use new RandomCharsTable().setDebug(true).run();
-    3 3 odd
-    | K | N | W |
-    | N | E | E |
-    | J | G | D |
-    odd letters - K, W, E, E, G
- */
+        for this test use new RandomCharsTable().setDebug(true).run();
+        3 3 odd
+        | K | N | W |
+        | N | E | E |
+        | J | G | D |
+        odd letters - K, W, E, E, G
+     */
     @Test
     void InputValidOddArguments() {
         setInput("3 3 odd");
@@ -57,5 +64,77 @@ class RandomCharsTableTest extends UnitBase {
         assertEquals("| N | E | E |", getOutputLines()[1]);
         assertEquals("| J | G | D |", getOutputLines()[2]);
         assertEquals("Odd letters - K, W, E, E, G", getOutputLines()[3]);
+    }
+
+    @Test
+    void InputFourRowsThreeColumnsAsArgument() {
+        setInput("4 3 odd");
+
+        new RandomCharsTable().run();
+        printOut();
+        removeFromOutput("Введите данные:");
+        assertTrue(getOutputLines()[0].matches("(\\|\\s[A-Z]\\s){3}\\|"));
+        assertTrue(getOutputLines()[1].matches("(\\|\\s[A-Z]\\s){3}\\|"));
+        assertTrue(getOutputLines()[2].matches("(\\|\\s[A-Z]\\s){3}\\|"));
+        assertTrue(getOutputLines()[3].matches("(\\|\\s[A-Z]\\s){3}\\|"));
+    }
+
+
+    // дальше тесты уродливы, но как иначе распарсить эти строки и сравнить все я не знаю
+    @Test
+    void InputCheckStrategyEven() {
+        final int ROWS = 3;
+        final int COLUMNS = 4;
+        final String STRATEGY = "even";
+        final String input = "" + ROWS + " " + COLUMNS + " " + STRATEGY;
+
+        setInput(input);
+
+        new RandomCharsTable().run();
+        printOut();
+        removeFromOutput("Введите данные:");
+
+        final String[] strategyRow = getOutputLines()[ROWS].trim().split(" - ");
+        final char[] evenCharsInAnswerRow = strategyRow[1].trim().replaceAll(", ","").toCharArray();
+        StringBuffer charsInTable = new StringBuffer();
+        for (int i = 0; i < ROWS; i++){
+            charsInTable.append(getOutputLines()[i].trim().replaceAll("\\|", "").replace(" ", ""));
+        }
+
+        for (char c : evenCharsInAnswerRow) {
+            assertEquals(0, c % 2);
+            assertNotEquals(-1, charsInTable.indexOf("" + c));
+        }
+        assertEquals("Even letters", strategyRow[0]);
+    }
+
+
+    // дальше тесты уродливы, но как иначе распарсить эти строки и сравнить все я не знаю
+    @Test
+    void InputCheckStrategyOdd() {
+        final int ROWS = 5;
+        final int COLUMNS = 4;
+        final String STRATEGY = "odd";
+        final String input = "" + ROWS + " " + COLUMNS + " " + STRATEGY;
+
+        setInput(input);
+
+        new RandomCharsTable().run();
+        printOut();
+        removeFromOutput("Введите данные:");
+
+        final String[] strategyRow = getOutputLines()[ROWS].trim().split(" - ");
+        final char[] oddCharsInAnswerRow = strategyRow[1].trim().replaceAll(", ","").toCharArray();
+
+        StringBuffer charsInTable = new StringBuffer();
+        for (int i = 0; i < ROWS; i++){
+            charsInTable.append(getOutputLines()[i].trim().replaceAll("\\|", "").replace(" ", ""));
+        }
+
+        for (char c : oddCharsInAnswerRow) {
+            assertEquals(1, c % 2);
+            assertNotEquals(-1, charsInTable.indexOf("" + c));
+        }
+        assertEquals("Odd letters", strategyRow[0]);
     }
 }
