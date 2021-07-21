2ce4e3ee2835fe13966233fb5d63f1a1bd295f0d
Anton.Segodnik <Anton.Segodnik@nordigy.ru>
2021-07-21 18:50:39 +0300

    homework_3

---
+++ src/main/java/homework_3/FinalClass.java
@@ -0,0 +1,29 @@
+package homework_3;
+
+// The class must be declared as final (So that child classes can’t be created)
+// Data members in the class must be declared as private (So that direct access is not allowed)
+// Data members in the class must be declared as final (So that we can’t change the value of it after object creation)
+// A parameterized constructor should initialize all the fields performing a deep copy (So that data members can’t be modified with object reference)
+// Deep Copy of objects should be performed in the getter methods (To return a copy rather than returning the actual object reference)
+// No setters (To not have the option to change the value of the instance variable)
+
+public final class FinalClass {
+
+   private final String string ;
+   private final int intVal;
+
+    private FinalClass(String string, int intVal) {
+        this.string = string;
+        this.intVal = intVal;
+    }
+
+    private FinalClass() {
+        this.string = "defaultVal";
+        this.intVal = 0;
+    }
+
+    private FinalClass createNewObj() {
+        return new FinalClass();
+    }
+
+}
