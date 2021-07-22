a2c06db0b80c0af40d8704fd83585e6a77725fdf
Frank Fibonacci <fadedfacet@gmail.com>
2021-07-22 21:36:25 +0300

    builder.gradle lombok added immutable class basic created

--- src/main/java/homework_3/ImmutableClass.java
+++ src/main/java/homework_3/ImmutableClass.java
@@ -1,5 +1,34 @@
 package homework_3;
 
-public class ImmutableClass {
+public final class ImmutableClass {
+    final String name;
+    final int number;
+
+    public ImmutableClass() {
+        this.name = "DefaultName";
+        this.number = 0;
+    }
+
+    public ImmutableClass(String str, int number) {
+        this.name = str;
+        this.number = number;
+    }
+
+    public ImmutableClass(int number, String str) {
+        this.number = number;
+        this.name = str;
+    }
+
+    public String getData() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public final Object newObjectMethod() {
+        return new ImmutableClass("fromMethodCreated", 1);
+    }
 
 }
