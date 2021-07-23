71399669826e554fe1cc66fd57b2177564645881
ijustwannatry <dmitrisedam11@gmail.com>
2021-07-23 17:41:07 +0300

    checkpoint

--- src/test/java/homework_2/pyramid_printer/PyramidPrinterTest.java
+++ src/test/java/homework_2/pyramid_printer/PyramidPrinterTest.java
@@ -3,14 +3,32 @@ package homework_2.pyramid_printer;
 import base.UnitBase;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PyramidPrinterTest extends UnitBase {
 
     @Test
-    void givenInts_whenRun_thenPrintPyramid() {
+    void givenOne_whenRun_thenPrintPyramid() {
         setInput("1");
         new PyramidPrinter().run();
-        final String res = getOutput();
+        printOut();
+        removeFromOutput("How many levels should the pyramid be?");
+
+        assertEquals("x", getOutputLines()[0]);
     }
 
+    @Test
+    void givenFive_whenRun_thenPrintPyramid() {
+        setInput("5");
+        new PyramidPrinter().run();
+        printOut();
+        removeFromOutput("How many levels should the pyramid be?");
+
+        assertEquals("x", getOutputLines()[0]);
+        assertEquals("xx", getOutputLines()[1]);
+        assertEquals("xxx", getOutputLines()[2]);
+        assertEquals("xxxx", getOutputLines()[3]);
+        assertEquals("xxxxx", getOutputLines()[4]);
+    }
 }
