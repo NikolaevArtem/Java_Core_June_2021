3ddfb53ad99cc4b7ae021be64b67c3812339829a
KhlebnikovEvgeniy <khebnikove@gmail.com>
2021-07-23 14:44:23 +0300

    HW2 edited 23.07.2021

---
+++ src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
@@ -0,0 +1,9 @@
+package homework_2.pyramid_printer;
+
+public class PyramidPrinter implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/homework_2/random_chars_table/RandomCharsTable.java
@@ -0,0 +1,9 @@
+package homework_2.random_chars_table;
+
+public class RandomCharsTable implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
