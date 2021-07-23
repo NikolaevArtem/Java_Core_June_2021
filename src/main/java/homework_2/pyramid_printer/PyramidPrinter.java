0670db50318ef79fcfaa5614bcbb25b123f98bc9
Elena Rog <rog.elena.yu@gmail.com>
2021-07-23 15:23:08 +0300

    homework 2

--- src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
+++ src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
@@ -1,34 +1,49 @@
 package homework_2.pyramid_printer;
 
-import java.util.InputMismatchException;
+import lombok.Getter;
+import lombok.Setter;
 import java.util.Scanner;
 
+@Getter
+@Setter
 class PyramidPrinter {
+    private StringBuilder pyramidString;
 
     public void run() {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
 
-        Scanner in = new Scanner(System.in);
         try {
-            int number = in.nextInt();
-            in.close();
-            StringBuilder result = new StringBuilder();
-            char x = 'x';
-            char cr = 13;
-            int i = 0;
-            int j;
-            while (i < number) {
-                j = 0;
-                while (j <= i) {
-                    result.append(x);
-                    j++;
-                }
-                result.append(cr);
-                i++;
-                System.out.println(result);
+            int number = Integer.parseInt(str);
+            if (number < 0) {
+                throw new ArithmeticException();
             }
-        } catch (
-                InputMismatchException e) {
+            buildPyramidString(number);
+            printPyramid();
+        } catch (ArithmeticException ex) {
+            System.out.println("Error! Only positive numbers!");
+        } catch (NumberFormatException ex) {
             System.out.println("Error! Only numbers!");
         }
+        scanner.close();
+    }
+
+    public void buildPyramidString(int number) {
+        StringBuilder result = new StringBuilder();
+        char charX = 'x';
+        char newLineChar = '\n';
+
+        for (int i = 0, j = 0; i < number; i++, j = 0) {
+            while (j <= i) {
+                result.append(charX);
+                j++;
+            }
+            result.append(newLineChar);
+        }
+        setPyramidString(result);
+    }
+
+    public void printPyramid() {
+        System.out.println(getPyramidString().toString());
     }
 }
