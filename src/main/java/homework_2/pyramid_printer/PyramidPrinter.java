a3c38f4bbbded9eb26dc84aee1bbcfe8225ecac4
Gamzat Asadulaev <asadulaevgamzat@gmail.com>
2021-07-24 05:05:48 +0300

    Update PyramidPrinter

--- src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
+++ src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
@@ -1,22 +1,29 @@
-package homework_2.homework2_2;
+package homework_2.pyramid_printer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class PyramidPrinter {
-    public static void main(String[] args)  {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public void run() {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Please input number");
             int rows = Integer.parseInt(reader.readLine());
             printPyramid(rows);
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (NumberFormatException e){
-            System.err.println("Only integer!");
+        } catch (IllegalArgumentException e) {
+            System.out.println(ANSI_RED + "Only 1 non-negative integer is allowed as passed parameter!" + ANSI_RESET);
         }
     }
 
-     static void printPyramid(int rows){
+    void printPyramid(int rows) {
+        if(rows < 0){
+            throw new IllegalArgumentException();
+        }
         for (int i = 1; i <= rows; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("x");
