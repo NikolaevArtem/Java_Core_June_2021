fcd59e3fe42f69002475265abf37032c79555fde
Frank Fibonacci <fadedfacet@gmail.com>
2021-07-24 19:44:32 +0300

    valid tests added

--- src/test/java/homework_2/random_chars_table/sources/RandomCharsTableTest.java
+++ src/test/java/homework_2/random_chars_table/sources/RandomCharsTableTest.java
@@ -2,6 +2,7 @@ package homework_2.random_chars_table.sources;
 
 import base.UnitBase;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RandomCharsTableTest extends UnitBase {
@@ -59,6 +60,44 @@ public class RandomCharsTableTest extends UnitBase {
         removeFromOutput("Input table length, table width, table strategy(even/odd): ");
         assertEquals("Passed parameters should match the format [positive integer] [positive integer] [even|odd]",
                 getOutputLines()[0]);
+    }
+
+    @Test
+    void testValidEven() {
+        setInput("5 5 even");
+
+        new RandomCharsTable().run();
+        printOut();
+        removeFromOutput("Input table length, table width, table strategy(even/odd): ");
+        for (int i = 0; i < 5; i++) {
+            String[] row = getOutputLines()[i].split(" ");
+            for (String el : row) {
+                assert (el.charAt(0) >= 65 && el.charAt(0) <= 90);
+            }
+        }
+        String[] result = getOutputLines()[5].split(" ");
+        for (int i = 3; i < result.length; i++) {
+            assert (result[i].charAt(0) % 2 == 0);
+        }
+    }
+
+    @Test
+    void testValidOdd() {
+        setInput("7 5 odd");
+
+        new RandomCharsTable().run();
+        printOut();
+        removeFromOutput("Input table length, table width, table strategy(even/odd): ");
+        for (int i = 0; i < 7; i++) {
+            String[] row = getOutputLines()[i].split(" ");
+            for (String el : row) {
+                assert (el.charAt(0) >= 65 && el.charAt(0) <= 90);
+            }
+        }
+        String[] result = getOutputLines()[7].split(" ");
+        for (int i = 3; i < result.length; i++) {
+            assert (result[i].charAt(0) % 2 == 1);
+        }
     }
 
 }
