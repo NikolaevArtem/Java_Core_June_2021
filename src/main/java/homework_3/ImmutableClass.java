f505079765bae73db14cebe8ece082f738afbf12
Frank Fibonacci <fadedfacet@gmail.com>
2021-07-24 23:15:01 +0300

    _

--- src/main/java/homework_3/ImmutableClass.java
+++ src/main/java/homework_3/ImmutableClass.java
@@ -2,10 +2,14 @@ package homework_3;
 
 /*
 Immutable class
-3 constuctors: (), (String, int, double), ("")
-3 getMethods(): getName, getAge, getWeight
-1 method returning a new obj: newObjectMethod()
+    The class must be declared as final (So that child classes can’t be created)
+    Data members in the class must be declared as private (So that direct access is not allowed)
+    Data members in the class must be declared as final (So that we can’t change the value of it after object creation)
+    A parameterized constructor should initialize all the fields performing a deep copy (So that data members can’t be modified with object reference)
+    Deep Copy of objects should be performed in the getter methods (To return a copy rather than returning the actual object reference)
+    No setters (To not have the option to change the value of the instance variable)
  */
+
 public final class ImmutableClass {
 
     private final String name; // reference type
