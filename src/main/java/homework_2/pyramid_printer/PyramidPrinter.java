515ed77939038d5ce148c0d6e43da17e4271b036
Frank Fibonacci <fadedfacet@gmail.com>
2021-07-25 16:00:14 +0300

    _

--- src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
+++ src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
@@ -16,8 +16,8 @@ public class PyramidPrinter {
     }
 
     private void printPyramid(int height) {
-        for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 1; j <= i + 1; j++) {
                 System.out.print("x");
             }
             System.out.println();
