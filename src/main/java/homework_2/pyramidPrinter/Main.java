579a708f80d4b397f12aa32ac0ad22c0f996adb7
arkadiy krylosov <arkady.krylosov@yandex.ru>
2021-07-25 04:29:40 +0300

    homework_2 corrected

--- src/main/java/homework_2/pyramidPrinter/Main.java
+++
@@ -1,8 +0,0 @@
-package homework_2.pyramidPrinter;
-
-public class Main {
-    public static void main(String[] args) {
-        PyramidPrinter pyramidPrinter = new PyramidPrinter();
-        pyramidPrinter.run();
-    }
-}
