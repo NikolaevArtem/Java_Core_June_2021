5d96e600cb613f681faadd25c75ba7908e0db689
vrodina <valeria.rodina@diabolocom.com>
2021-07-25 18:18:02 +0300

    Move task to new ExtraTrafficLight class Add TrafficLight task Fix Main class with choosing mode

--- src/main/java/homework_2/traffic_light/Main.java
+++ src/main/java/homework_2/traffic_light/Main.java
@@ -2,6 +2,10 @@ package homework_2.traffic_light;
 
 public class Main {
     public static void main(String[] args) {
-        new TrafficLight().run();
+        if (args[0].equals("ExtraMode")) {
+            new ExtraTrafficLight().run();
+        } else {
+            new TrafficLight().run();
+        }
     }
 }
