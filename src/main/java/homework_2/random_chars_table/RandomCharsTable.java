bcf657b218f9490e6e97ccb7678aa676f21fe47e
Waranko <mari.waranko@gmail.com>
2021-07-25 04:14:27 +0300

    RandomCharsTable add classes

--- src/main/java/homework_2/random_chars_table/RandomCharsTable.java
+++ src/main/java/homework_2/random_chars_table/RandomCharsTable.java
@@ -7,30 +7,64 @@ import java.util.ArrayList;
 
 public class RandomCharsTable {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputValues = br.readLine().split(" ");
+    private final String ERROR_MESSAGE = "Passed parameters should match the format [positive integer] [positive integer] [even|odd]";
+    private boolean isEven;
+    private char[][] randomCharsTable;
+    private final ArrayList<Character> selectedChars = new ArrayList<>();
 
-        int rows = Integer.parseInt(inputValues[0]);
-        int columns = Integer.parseInt(inputValues[1]);
-        boolean isEven = inputValues[2].equals("even");
-        if (!isEven && !inputValues[2].equals("odd")) {
-            System.out.println("Input error: Unrecognized strategy!");
+    public void run() {
+        System.out.println("Please, enter array length, array width and strategy (odd or even):");
+
+        int rows, columns;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String[] inputValues = br.readLine().split(" ");
+            if(inputValues.length != 3){
+                System.out.println(ERROR_MESSAGE);
+                return;
+            }
+            rows = Integer.parseInt(inputValues[0]);
+            columns = Integer.parseInt(inputValues[1]);
+            if(rows <= 0 || columns <= 0){
+                System.out.println(ERROR_MESSAGE);
+                return;
+            }
+            isEven = inputValues[2].equals("even");
+            if (!isEven && !inputValues[2].equals("odd")) {
+                System.out.println(ERROR_MESSAGE);
+                return;
+            }
+        } catch (IOException | NumberFormatException e) {
+            System.out.println(ERROR_MESSAGE);
             return;
         }
 
-        ArrayList<Character> selectedChars = new ArrayList<>();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                char ch = (char) ('A' + Math.random() * 26);
+        randomCharsTable = new RandomCharsTableCreator(rows, columns).createTable();
+        findSelectedChars();
+        printTable();
+        printSelection();
+    }
+
+    private void findSelectedChars(){
+        for (char[] row : randomCharsTable) {
+            for (char ch : row) {
                 if ((isEven && (int) ch % 2 == 0) || (!isEven && (int) ch % 2 == 1)) {
                     selectedChars.add(ch);
                 }
+            }
+        }
+    }
+
+    private void printTable(){
+        for (char[] row : randomCharsTable) {
+            System.out.print("|");
+            for (char ch : row) {
                 System.out.print(ch + "|");
             }
             System.out.println();
         }
+    }
 
+    private void printSelection(){
         if (isEven) {
             System.out.print("Even letters - ");
         } else {
