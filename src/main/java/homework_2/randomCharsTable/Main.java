88d84b06b540d183a461dd9ea20dfc9e68443d75
arkadiy krylosov <arkady.krylosov@yandex.ru>
2021-07-25 04:29:40 +0300

    homework_2 corrected

--- src/main/java/homework_2/randomCharsTable/Main.java
+++
@@ -1,8 +0,0 @@
-package homework_2.randomCharsTable;
-
-public class Main {
-    public static void main(String[] args) {
-        RandomCharsTable randomCharsTable = new RandomCharsTable();
-        randomCharsTable.run();
-    }
-}
