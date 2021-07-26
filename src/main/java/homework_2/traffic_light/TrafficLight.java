38d7e094676535f694c36e891f96e8bb8d8ead3d
Waranko <mari.waranko@gmail.com>
2021-07-26 19:09:32 +0300

    TrafficLight tests

--- src/main/java/homework_2/traffic_light/TrafficLight.java
+++ src/main/java/homework_2/traffic_light/TrafficLight.java
@@ -6,53 +6,42 @@ import java.io.InputStreamReader;
 
 public class TrafficLight {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Choose input mode:\n1 - number of seconds\n2 - hh:mm:ss");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String answer = br.readLine();
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String START_MESSAGE = "Please, input number of seconds:";
+    public static final String ERROR_MESSAGE = "Only 1 non-negative integer is allowed as passed parameter";
+    private int seconds;
 
-        int seconds;
-        if(answer.equals("1")){
-            try {
-                seconds = Integer.parseInt(br.readLine());
-            } catch (NumberFormatException nfe) {
-                System.out.println("Input error: The number of seconds can be a number only!");
-                return;
-            }
-        } else if(answer.equals("2")){
-            String[] time = br.readLine().split(":");
-            if(time.length != 3){
-                System.out.println("Input error: Wrong input format!");
-                return;
-            }
-            try {
-                int hours = Integer.parseInt(time[0]);
-                int minutes = Integer.parseInt(time[1]);
-                seconds = 60 * 60 * hours + 60 * minutes + Integer.parseInt(time[2]);
-            } catch (NumberFormatException nfe) {
-                System.out.println("Input error: Values can be numbers only!");
-                return;
-            }
-        } else{
-            System.out.println("Unrecognizable answer!");
+    public void run() {
+        System.out.println(START_MESSAGE);
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            seconds = Integer.parseInt(br.readLine());
+        } catch (IOException | NumberFormatException e) {
+            System.out.println(ERROR_MESSAGE);
             return;
         }
-
         if (seconds < 0) {
-            System.out.println("Input error: The number of seconds should be positive!");
+            System.out.println(ERROR_MESSAGE);
+            return;
+        }
+        if (seconds > 86399) {
+            System.out.println(ERROR_MESSAGE);
             return;
         }
 
-        if (seconds > 86399) {
-            System.out.println("Input error: The number of seconds is too big. The day is already over!");
-            return;
-        }
+        printColor();
+    }
+
+    private void printColor() {
         if (seconds % 60 < 35) {
-            System.out.println("Green");
+            System.out.println(GREEN + "GREEN" + RESET);
         } else if (seconds % 60 < 40 || seconds % 60 >= 55) {
-            System.out.println("Yellow");
+            System.out.println(YELLOW + "YELLOW" + RESET);
         } else {
-            System.out.println("Red");
+            System.out.println(RED + "RED" + RESET);
         }
     }
 }
---
+++ src/test/java/homework_2/TrafficLightTest.java
@@ -0,0 +1,91 @@
+package homework_2;
+
+import homework_2.traffic_light.TrafficLight;
+import org.junit.jupiter.api.Test;
+
+import static homework_2.traffic_light.TrafficLight.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrafficLightTest extends base.UnitBase {
+
+    private final String RED_VALUE = RED.substring(1);
+    private final String YELLOW_VALUE = YELLOW.substring(1);
+    private final String GREEN_VALUE = GREEN.substring(1);
+
+    @Test
+    void getGreenTest() {
+        setInput("5");
+
+        new TrafficLight().run();
+        printOut();
+        removeFromOutput(START_MESSAGE);
+
+        assertEquals(GREEN_VALUE + "GREEN" + RESET, getOutput());
+    }
+
+    @Test
+    void getYellowTest() {
+        setInput("35");
+
+        new TrafficLight().run();
+        printOut();
+        removeFromOutput(START_MESSAGE);
+
+        assertEquals(YELLOW_VALUE + "YELLOW" + RESET, getOutput());
+    }
+
+    @Test
+    void getRedTest() {
+        setInput("54");
+
+        new TrafficLight().run();
+        printOut();
+        removeFromOutput(START_MESSAGE);
+
+        assertEquals(RED_VALUE + "RED" + RESET, getOutput());
+    }
+
+    @Test
+    void nullInputTest() {
+        setInput("0");
+
+        new TrafficLight().run();
+        printOut();
+        removeFromOutput(START_MESSAGE);
+
+        assertEquals(GREEN_VALUE + "GREEN" + RESET, getOutput());
+    }
+
+    @Test
+    void tooBigInputTest() {
+        setInput("86401");
+
+        new TrafficLight().run();
+        printOut();
+        removeFromOutput(START_MESSAGE);
+
+        assertEquals(ERROR_MESSAGE, getOutput());
+    }
+
+    @Test
+    void stringInputTest() {
+        setInput("string");
+
+        new TrafficLight().run();
+        printOut();
+        removeFromOutput(START_MESSAGE);
+
+        assertEquals(ERROR_MESSAGE, getOutput());
+    }
+
+    @Test
+    void charInputTest() {
+        setInput("c");
+
+        new TrafficLight().run();
+        printOut();
+        removeFromOutput(START_MESSAGE);
+
+        assertEquals(ERROR_MESSAGE, getOutput());
+    }
+}
