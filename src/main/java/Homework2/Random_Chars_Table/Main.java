28f56f967352cc70a161291678fd317d2a17a7e9
Daisy <la.reine.m@gmai.com>
2021-07-26 04:08:14 +0300

    Makes and prints the table, calculates odds and evens. Misprints yet

--- src/main/java/Homework2/Random_Chars_Table/Main.java
+++ src/main/java/Homework2/Random_Chars_Table/Main.java
@@ -4,15 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class Main {
+    static int length = 0;
+    static int height = 0;
+
     public static void main(String[] args) throws IOException {
-        int length = 0;
-        int height = 0;
         String strategy = "";
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = reader.readLine();
-        if (input.matches("\\d+\\s\\d+\\sodd") || input.matches("\\d+\\s\\d+\\seven")) {
+        if (input.matches("\\d+\\s\\d+\\sodd") || input.matches("\\d+\\s\\d+\\seven")
+                && height >= 0 && length >= 0) {
             String[] split = input.split(" ");
             length = Integer.parseInt(split[0]);
             height = Integer.parseInt(split[1]);
@@ -20,32 +23,43 @@ public class Main {
         } else {
             System.out.println("The input line does not match the pattern");
         }
-
+        StringBuilder builder = new StringBuilder();
         RandomCharsTable randomCharsTable = new RandomCharsTable();
-        char[][] randomCharTable = randomCharsTable.getRandomCharTable(length, height);
-        for (char[] c : randomCharTable) {
-            System.out.println(c);
-//            if (strategy.equals("odd")) {
-//                {
-//                    for (int i = 0; i < length; i++) {
-//                        for (int j = 0; j < height; j++) {
-//                            if (randomCharTable[i][j] % 2 != 0) {
-//                                System.out.println("Odd letters - " + randomCharTable[i][j] + ", ");
-//                            }
-//                        }
-//                    }
-//                }
-//
-//            } else if (strategy.equals("even")) {
-//                for (int i = 0; i < length; i++) {
-//                    for (int j = 0; j < height; j++) {
-//                        if (randomCharTable[i][j] % 2 == 0) {
-//                            System.out.println("Odd letters - " + randomCharTable[i][j] + ", ");
-//                        }
-//                    }
-//                }
-//            }
+        char[][] rCT = RandomCharsTable.getRandomCharsTable(length, height);
+
+        for (char[] ch : rCT) {
+            builder.append("| ");
+            for (char c : ch) {
+                builder.append(c).append(" | ");
+            }
+            builder.append("\n");
         }
+        System.out.println(builder.toString());
+
+        if(strategy.equals("odd")){
+            builder.append("Odd letters - ");
+            for (int i = 0; i < length; i++){
+                for (int j = 0; j < height; j++){
+                    if (rCT[i][j]% 2 != 0){
+                        builder.append(rCT[i][j]).append(" ");
+                    }
+                }
+            }
+        }
+        System.out.println(builder.toString());
+
+        if (strategy.equals("even")){
+            builder.append("Even letters - ");
+            for (int i = 0; i < length; i++){
+                for (int j = 0; j < height; j++){
+                    if (rCT[i][j] % 2 == 0) {
+                        builder.append(rCT[i][j]).append(" ");
+                    }
+                }
+            }
+
+        }
+        System.out.println(builder.toString());
     }
 
 }
--- src/main/java/Homework2/Random_Chars_Table/RandomCharsTable.java
+++ src/main/java/Homework2/Random_Chars_Table/RandomCharsTable.java
@@ -1,49 +1,51 @@
 package Homework2.Random_Chars_Table;
 /*
-Создать приложение "RandomCharsTable"
-
-Считывает 2 числа и строку - длину и ширину таблицы/двумерного массива,
-и стратегию (even или odd).
+3) Создать приложение "RandomCharsTable"
+Считывает один раз, в одну строчку 2 числа и строку - длину и ширину таблицы/двумерного массива,
+и стратегию (even или odd)
 Печатает таблицу, заполненную случайно сгенерированными буквами от A до Z.
-
-Печатает в одну строку, через запятую, все четные или нечетные буквы в таблице
-в зависимости от выбранной стратегии (A = 65, B = 66 и т.д.)
-
+Печатает в одну строку, через запятую, все четные или нечетные буквы в таблице в зависимости
+от выбранной стратегии (A = 65, B = 66 и т.д.)
 AC:
+
 input - 2 2 even, output -
 		| A | E |
 		| G | D |
 		Even letters - G, D
+
 input - 2 3 odd, output -
 		| L | P | X |
 		| S | N | Q |
 		Odd letters - S, Q
+Любая ошибка с некорректным вводом/0 на месте длины_ширины/числом параметров:
+output - Passed parameters should match the format [positive integer] [positive integer] [even|odd]
+Доп. функционал можно сделать выбором мода при старте приложения (через args)
  */
 
 public class RandomCharsTable {
 
-    static char randomChar   (int min, int max) {
+    //получаю случайные числа
+    static char getRandomChar(int min, int max) {
         min = 65;
         max = 90;
         max -= min;
-
         char randomCh = (char) ((int) (Math.random() * ++max) + min);
-
         return randomCh;
     }
 
-     char [][] getRandomCharTable (int length, int height){
+    //складываю случайые числа в массив
+    public static char [][] getRandomCharsTable(int length, int height){
         char [][] randomCharTable = new char[length][height];
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < height; j++) {
-               char ch = randomChar(65, 90);
+               char ch = getRandomChar(65, 90);
                randomCharTable[i][j] = ch;
             }
         }
         return randomCharTable;
     }
-    public void printRandomChartable(char getRand){
 
-    }
 
 }
+
+
