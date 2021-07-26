ca5976baed6bae56663a088ec0d1239bebc2a55b
GabdrakhimovaRR <gabdrakhimovarr@gmail.com>
2021-07-26 07:58:56 +0300

    homework_1(fixed indentation)

--- src/main/java/homework_1/Main.java
+++ src/main/java/homework_1/Main.java
@@ -1,17 +1,17 @@
 package homework_1;
 
 public class Main {
-  static final String RED = "\u001b[31m";
-  static final String RESET = "\u001b[0m";
+    static final String RED = "\u001b[31m";
+    static final String RESET = "\u001b[0m";
 
-  public static void main(String[] args) {
-      for (int i = 0; i < args.length; i++) {
-      if (args[i].equals("error")) {
-        System.out.println(RED + "Alarm!"+ RESET);
-        break;
-      } else {
-        System.out.println(args[i] + ": " + args[i].length() + " letters");
-      }
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("error")) {
+                System.out.println(RED + "Alarm!"+ RESET);
+                break;
+            } else {
+                System.out.println(args[i] + ": " + args[i].length() + " letters");
+            }
+        }
     }
-  }
 }
