02e9fa102b573b008ad02d19b8c6031bfe773a5b
Artem Nikolaev <anikolaev@ebsco.com>
2021-07-26 14:31:07 +0500

    Revert changes in lesson_1

--- src/main/java/lesson_1/Main.java
+++ src/main/java/lesson_1/Main.java
@@ -3,20 +3,7 @@ package lesson_1;
 public class Main {
 
   public static void main(String[] args) {
-    final Cat cat1 = new Cat();
-    final Cat cat2 = new Cat();
-    final Cat cat3 = new Cat();
-
-//    System.out.println(cat1.getLegs());
-//    System.out.println(cat2.getLegs());
-//    System.out.println(cat3.getLegs());
-//
-//    cat2.setLegs("five");
-//
-//    System.out.println(cat1.getLegs());
-//    System.out.println(cat2.getLegs());
-//    System.out.println(cat3.getLegs());
-
+    System.out.println("Hello World!");
   }
 
 }
