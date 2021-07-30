f741feec9cbafb3deee00d24d04658ad0db71840
OzoneTaTaY <volchenkovolk@mail.ru>
2021-07-30 19:50:09 +0300

    Split HW_2 into Main and Class with run()

---
+++ src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
@@ -0,0 +1,33 @@
+package homework_2.pyramid_printer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PyramidPrinter {
+
+    public void run() {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Enter height of pyramid:");
+            String inputStr = bufferedReader.readLine();
+            try {
+                int height = Integer.parseInt(inputStr);
+                if (height > 0) {
+                    for (int i = 0; i < height; i++) {
+                        for (int j = 0; j < i + 1; j++) {
+                            System.out.print("x");
+                        }
+                        System.out.println();
+                    }
+                } else {
+                    System.err.println("Out of range (input should be >= 0)");
+                }
+                bufferedReader.close();
+            } catch (NumberFormatException e) {
+                System.err.println("Can't parse input string to integer");
+            }
+        } catch (IOException e) {
+            System.err.println("IOException has caught");
+        }
+    }
+}
