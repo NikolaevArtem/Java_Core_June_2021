7796db23069602d8255ab8a75d489247d87bf8dc
pepyachka <ndrozdov9@gmail.com>
2021-08-04 01:30:10 +0300

    add hw4 CustomAnnotation

---
+++ src/main/java/homework_4/custom_annotation/custom_exception/XmlSerializationException.java
@@ -0,0 +1,8 @@
+package homework_4.custom_annotation.custom_exception;
+
+public class XmlSerializationException extends RuntimeException {
+
+    public XmlSerializationException(String message) {
+        super(message);
+    }
+}
