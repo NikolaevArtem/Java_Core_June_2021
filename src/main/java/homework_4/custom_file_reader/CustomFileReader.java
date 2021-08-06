d1fd579934676dc7de84d3d18606bcfdc98b5fb6
vrodina <valeria.rodina@diabolocom.com>
2021-08-07 01:33:37 +0300

    Add CustomFileReader carcass and new structure for hw4

---
+++ src/main/java/homework_4/custom_file_reader/CustomFileReader.java
@@ -0,0 +1,15 @@
+package homework_4.custom_file_reader;
+
+public class CustomFileReader {
+    public void run1() {
+    }
+
+    public void run2() {
+    }
+
+    public void run3() {
+    }
+
+    public void run4() {
+    }
+}
