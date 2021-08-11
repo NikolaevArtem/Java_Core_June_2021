14cc3d0edbf38ad38802e194cdbd8f2e86fc5866
Kollega1984 <bomba_25@mail.ru>
2021-08-11 20:01:45 +0300

    run 1 completed

--- src/main/java/homework_4/custom_file_reader/CustomFileReader.java
+++ src/main/java/homework_4/custom_file_reader/CustomFileReader.java
@@ -8,14 +8,14 @@ import java.nio.file.Paths;
 
 public class CustomFileReader {
     public void run() {
-        Path filePath = Paths.get("main/resources/custom_file_reader/test_file.txt");
+        Path filePath = Paths.get("C:\\Java_Core_June_2021\\src\\main\\resources\\custom_file_reader\\test_file");
 
         if (Files.exists(filePath)) {
             try {
                 byte[] bytes = Files.readAllBytes(filePath);
                 String text = new String(bytes, StandardCharsets.UTF_8);
-
-                System.out.println(text);
+                String resul = text.replaceAll("[,.]", "");
+                System.out.println(resul);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
