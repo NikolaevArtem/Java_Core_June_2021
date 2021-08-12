b7edb8ee8be4dd0922deb3a46efd140dbebae4f4
Waranko <mari.waranko@gmail.com>
2021-08-12 21:37:59 +0300

    HW4 CustomFileReader done

---
+++ src/main/java/homework_4/custom_file_reader/Main.java
@@ -0,0 +1,14 @@
+package homework_4.custom_file_reader;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        CustomFileReader cfr = new CustomFileReader();
+        cfr.run1();
+        cfr.run2();
+        cfr.run3();
+    }
+
+}
