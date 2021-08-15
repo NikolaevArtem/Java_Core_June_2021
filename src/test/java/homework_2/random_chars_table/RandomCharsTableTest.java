149de9abf1aa7dc846af4f160d8e8bbfb3a29725
KhlebnikovEvgeniy <khebnikove@gmail.com>
2021-08-15 18:50:45 +0300

    Java_Core_June_2021 project restored 31.07.2021

--- src/test/java/homework_2/random_chars_table/RandomCharsTableTest.java
+++ src/test/java/homework_2/random_chars_table/RandomCharsTableTest.java
@@ -3,39 +3,117 @@ package homework_2.random_chars_table;
 import base.UnitBase;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RandomCharsTableTest extends UnitBase {
 
+    public final String WELCOME_MESSAGE_TEST = "Welcome to the Random Chars Table Application";
+    public final String ENTER_PARAM_MESSAGE_TEST = "Enter in one line 2 numbers and a line - " +
+            "the length and width of the table / two-dimensional array," +
+            " and the strategy (even or odd)";
+    public final String ERROR_MESSAGE_TEST = "Passed parameters should match the format " +
+            "[positive integer] [positive integer] [even|odd]";
+
     @Test
-    void run() {
+    void givenZeroAsArgument_whenRun_thenErrorMessage() {
+        setInput("0 0 odd");
+        new RandomCharsTable().run();
+        printOut();
+        removeFromOutput(WELCOME_MESSAGE_TEST);
+        removeFromOutput(ENTER_PARAM_MESSAGE_TEST);
+        assertEquals(ERROR_MESSAGE_TEST, getOutputLines()[0]);
     }
 
     @Test
-    void startGreeting() {
+    void givenEmptyArgument_whenRun_thenNoSuchElementException() {
+        setInput("");
+        assertThrows(NoSuchElementException.class, () -> new RandomCharsTable().run());
     }
 
     @Test
-    void readConsoleString() {
+    void givenNegativeIntegerAsFirstArgument_whenRun_thenPrintErrorMessage() {
+        setInput("-2 2 even");
+        new RandomCharsTable().run();
+        assertTrue(getOutput().contains(ERROR_MESSAGE_TEST));
     }
 
     @Test
-    void printOddCapitalLetters() {
+    void givenNnegativeIntegerAsSecondArgument_whenRun_thenPrintErrorMessage() {
+        setInput("2 -2 even");
+        new RandomCharsTable().run();
+        assertTrue(getOutput().contains(ERROR_MESSAGE_TEST));
     }
 
     @Test
-    void printEvenCapitalLetters() {
+    void givenWrongStrategy_whenRun_thenPrintErrorMessage() {
+        setInput("2 2 wrongStrategy");
+        new RandomCharsTable().run();
+        assertTrue(getOutput().contains(ERROR_MESSAGE_TEST));
     }
 
     @Test
-    void printCapitalLetters() {
+    void givenLetterAsFirstArgument_whenRun_thenPrintErrorMessage() {
+        setInput("w 2 even");
+        new RandomCharsTable().run();
+        assertTrue(getOutput().contains(ERROR_MESSAGE_TEST));
     }
 
     @Test
-    void printTable() {
+    void givenLetterAsSecondrgument_whenRun_thenPrintErrorMessage() {
+        setInput("2 W even");
+        new RandomCharsTable().run();
+        assertTrue(getOutput().contains(ERROR_MESSAGE_TEST));
     }
 
     @Test
-    void solveTask() {
+    void givenDoubleAsFirstArgument_whenRun_thenPrintErrorMessage() {
+        setInput("0.12345 2 even");
+        new RandomCharsTable().run();
+        assertTrue(getOutput().contains(ERROR_MESSAGE_TEST));
     }
+
+    @Test
+    void givenDoubleAsSecondArgument_whenRun_thenPrintErrorMessage() {
+        setInput("2 0.12345 even");
+        new RandomCharsTable().run();
+        assertTrue(getOutput().contains(ERROR_MESSAGE_TEST));
+    }
+
+    @Test
+    void givenMoreThanThreeArguments_whenRun_thenPrintErrorMessage() {
+        setInput("1 2 3 odd");
+        new RandomCharsTable().run();
+        assertTrue(getOutput().contains(ERROR_MESSAGE_TEST));
+    }
+
+    @Test
+    void givenLengthWidthEvenStrategy_whenRun_thenPrintEvenChars() {
+        setInput("8 8 even");
+        new RandomCharsTable().run();
+        removeFromOutput("Even letters - ");
+        removeFromOutput(",");
+        removeFromOutput(" ");
+        String str = getOutputLines()[getOutputLines().length - 1];
+        for (int i = 0; i < str.length(); i++) {
+            assertEquals(0, str.charAt(i) % 2);
+        }
+    }
+
+
+    @Test
+    void givenLengthWidthOddStrategy_whenRun_thenPrintOddChars() {
+        setInput("8 8 odd");
+        new RandomCharsTable().run();
+        removeFromOutput("Odd letters - ");
+        removeFromOutput(",");
+        removeFromOutput(" ");
+        String str = getOutputLines()[getOutputLines().length - 1];
+        for (int i = 0; i < str.length(); i++) {
+            assertEquals(1, str.charAt(i) % 2);
+        }
+    }
+
+
 }
