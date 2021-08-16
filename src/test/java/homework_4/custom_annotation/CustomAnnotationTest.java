94eebc91291984c618bc994186a6348c68444ba3
roman <roman.s.popov@gmail.com>
2021-08-16 12:49:09 +0300

    HW4 tests

--- src/test/java/homework_4/custom_annotation/CustomAnnotationTest.java
+++ src/test/java/homework_4/custom_annotation/CustomAnnotationTest.java
@@ -1,4 +1,78 @@
 package homework_4.custom_annotation;
 
-public class CustomAnnotationTest {
+import base.UnitBase;
+import homework_4.custom_file_reader.CustomFileReader;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CustomAnnotationTest extends UnitBase {
+
+    @Test
+    void givenRun1_whenSearch_thenTrue() {
+
+        try {
+            Method m = new CustomFileReader("").getClass().getMethod("run1");
+            assertTrue(m.isAnnotationPresent(CustomAnnotation.class));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void givenRun2_whenSearch_thenTrue() {
+
+        try {
+            Method m = new CustomFileReader("").getClass().getMethod("run2");
+            assertTrue(m.isAnnotationPresent(CustomAnnotation.class));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void givenRun3_whenSearch_thenTrue() {
+
+        try {
+            Method m = new CustomFileReader("").getClass().getMethod("run3");
+            assertTrue(m.isAnnotationPresent(CustomAnnotation.class));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void givenRun1_whenRun_thenTrue() {
+        new CustomFileReader("").run1("/custom_file_reader/file.txt");
+        printOut();
+        String expected = getOutputLines()[0];
+        new CustomFileReader("file.txt").run("run1");
+        printOut();
+        String actual = getOutputLines()[0];
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void givenRun2_whenRun_thenTrue() {
+        new CustomFileReader("").run2("src/main/resources/custom_file_reader/file.txt");
+        printOut();
+        String expected = getOutputLines()[0];
+        new CustomFileReader("file.txt").run("run2");
+        printOut();
+        String actual = getOutputLines()[0];
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void givenRun3_whenRun_thenTrue() {
+        new CustomFileReader("").run3("src/main/resources/custom_file_reader/file.txt");
+        printOut();
+        String expected = getOutputLines()[0];
+        new CustomFileReader("file.txt").run("run3");
+        printOut();
+        String actual = getOutputLines()[0];
+        assertEquals(expected, actual);
+    }
 }
--- src/test/java/homework_4/singleton/SingletonTest.java
+++ src/test/java/homework_4/singleton/SingletonTest.java
@@ -1,4 +1,27 @@
 package homework_4.singleton;
 
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SingletonTest {
+
+    @Before
+    public void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        Field instance = Singleton.class.getDeclaredField("instance");
+        instance.setAccessible(true);
+        instance.set(null, null);
+    }
+
+    @Test
+    void givenSingleton_whenGetInstance_thenSame() {
+
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        assertEquals(s1,s2);
+    }
 }
