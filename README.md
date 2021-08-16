8fdd9e87705cee477facfb0eabb1d1fcc5482aa5
Nikita <sotheres@gmail.com>
2021-08-16 14:20:01 +0300

    HW4 CustomFileReader tests update

--- README.md
+++ README.md
@@ -5,9 +5,14 @@
 | Number | Solution  | Short description
 | --- | --- | --- |
 | HW1 | [Console printer](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_1) | The app that reads input arguments and prints them, until "error" argument |
-| HW2 | [Pyramid Printer](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_2/pyramid_printer) | The app prints pyramid of entered height with "x" symbols|
-| HW2 | [Traffic Light](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_2/traffic_light) | The app shows traffic light corresponding to entered time of the day|
-| HW2 | [Random Chars Table](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_2/random_chars_table) | The app prints table of randomized letters and outputs list of odd/even letters in it|
+| HW2.1 | [Pyramid Printer](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_2/pyramid_printer) | The app prints pyramid of entered height with "x" symbols|
+| HW2.2 | [Traffic Light](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_2/traffic_light) | The app shows traffic light corresponding to entered time of the day|
+| HW2.3 | [Random Chars Table](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_2/random_chars_table) | The app prints table of randomized letters and outputs list of odd/even letters in it|
+| HW2 Tests | [Tests](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/test/java/homework_2_test/) | Tests for homework 2|
 | HW3 | [Immutable Class](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_3) | Cafe class that shows immutable class' key features |
+| HW4.1 | [Custom Annotation](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_4/custom_annotation) | Default User Info declares custom annotation and Profile class uses it|
+| HW4.2 | [Custom File Reader](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_4/custom_file_reader) | The app reads text from file and outputs it without dots and commas|
+| HW4.3 | [Singleton](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/main/java/homework_4/singleton) | Theater class as an example of singleton pattern|
+| HW4 Tests | [Tests](https://github.com/NikolaevArtem/Java_Core_June_2021/tree/feature/ChertovNikita/src/test/java/homework_4_test/) | Tests for homework 4|
 
 [Link to my CodingBat](https://codingbat.com/done?user=sotheres@gmail.com&tag=6008519258)
--- src/main/java/homework_4/custom_file_reader/CustomFileReader.java
+++ src/main/java/homework_4/custom_file_reader/CustomFileReader.java
@@ -1,27 +1,55 @@
 package homework_4.custom_file_reader;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.*;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class CustomFileReader {
 
-    private final String filePath = "src/main/resources/custom_file_reader/";
+    private final String pathToFile = "src/main/resources/custom_file_reader/";
     private final String sourceFile;
 
     public CustomFileReader() {
-        String defaultFileName = "defaultInput.txt";
-        sourceFile = filePath + defaultFileName;
+        String defaultFileName = "file.txt";
+        sourceFile = pathToFile + defaultFileName;
     }
 
     public CustomFileReader(String fileName) {
-        sourceFile = filePath + fileName;
+        sourceFile = pathToFile + fileName;
     }
 
-    void run1() {
+    public void run1() {
+        readByByte();
+    }
+    
+    public void run2() {
+        readBytesInBuffer();
+    }
+    
+    public void run3() {
+        readCharsInBuffer();
+    }
+
+    public void run4() {
+        readCharsFromCharacterStream();
+    }
+    
+    public void run5() {
+        readWithScanner();
+    }
+
+    public void run6() {
+        readWithNIO();
+    }
+
+    public void run7() {
+        readWithNIO2();
+    }
+
+    private void readByByte() {
         try (FileInputStream inputStream = new FileInputStream(sourceFile)) {
             int charsAvailable = inputStream.available();
             StringBuilder readText = new StringBuilder();
@@ -33,73 +61,103 @@ public class CustomFileReader {
                 charsAvailable--;
             }
             System.out.println(readText);
+        } catch (FileNotFoundException exc) {
+            System.out.println("File not found");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void run2() {
-        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(sourceFile))) {
+    private void readBytesInBuffer() {
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(
+                new FileInputStream(sourceFile))) {
             int charsAvailable = bufferedInputStream.available();
             byte[] input = new byte[charsAvailable];
-            if (bufferedInputStream.read(input) != -1) {
+            int byteRead = bufferedInputStream.read(input);
+            if (byteRead != -1) {
                 String readText = new String(input);
-                readText = readText.replace(".", "").replace(",", "");
+                readText = removePunctuation(readText);
                 System.out.println(readText);
             }
+        } catch (FileNotFoundException exc) {
+            System.out.println("File not found");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void run3() {
+    private void readCharsInBuffer() {
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
                         new FileInputStream(sourceFile)))) {
             String readText = reader.readLine();
             while (readText != null) {
-                readText = readText.replace(".", "").replace(",", "");
+                readText = removePunctuation(readText);
                 System.out.println(readText);
                 readText = reader.readLine();
             }
+        } catch (FileNotFoundException exc) {
+            System.out.println("File not found");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void run4() {
+    private void readCharsFromCharacterStream() {
         try (BufferedReader reader = new BufferedReader(new FileReader(sourceFile))) {
             String readText = reader.readLine();
             while (readText != null) {
-                readText = readText.replace(".", "").replace(",", "");
+                readText = removePunctuation(readText);
                 System.out.println(readText);
                 readText = reader.readLine();
             }
+        } catch (FileNotFoundException exc) {
+            System.out.println("File not found");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void run5() {
+    private void readWithScanner() {
         try (Scanner scanner = new Scanner(new File(sourceFile))) {
             while (scanner.hasNext()) {
                 String readText = scanner.nextLine();
-                readText = readText.replace(".", "").replace(",", "");
+                readText = removePunctuation(readText);
                 System.out.println(readText);
             }
         } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+
+    private void readWithNIO() {
+        try (FileChannel fileChannel = new FileInputStream(sourceFile).getChannel()) {
+            int availableBytes = (int) fileChannel.size();
+            ByteBuffer buffer = ByteBuffer.allocate(availableBytes);
+            fileChannel.read(buffer);
+            String readText = new String(buffer.array());
+            readText = removePunctuation(readText);
+            System.out.println(readText);
+        } catch (FileNotFoundException exc) {
+            System.out.println("File not found");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void run6() {
+    private void readWithNIO2() {
         Path path = Paths.get(sourceFile);
-        try {
-            Stream<String> lines = Files.lines(path);
-            lines = lines.map(n -> n = n.replaceAll("[,.]", ""));
-            lines.forEach(System.out::println);
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.map(n -> n = n.replaceAll("[,.]", ""))
+                    .forEach(System.out::println);
+        } catch (NoSuchFileException exc) {
+            System.out.println("File not found");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String removePunctuation(String text) {
+        return text.replace(".", "").replace(",", "");
     }
 }
