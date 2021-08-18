9ea484907f9904c122b7cdd4db2bbd1a0d60f0e9
Frank Fibonacci <fadedfacet@gmail.com>
2021-08-18 13:48:40 +0300

    prints method name fix;

--- src/main/java/homework_5/armstrong/Armstrong.java
+++ src/main/java/homework_5/armstrong/Armstrong.java
@@ -9,9 +9,9 @@ public class Armstrong {
         int b = 370;
         int c = 12;
         int d = 1234567;
-        System.out.println("arm(135, numLen(135)) = " + (long) arm(a, digitCounter(a)));
-        System.out.println("arm(370, numLen(370)) = " + (long) arm(b, digitCounter(b)));
-        System.out.println("arm(12, numLen(12)) = " + (long) arm(c, digitCounter(c)));
+        System.out.println("arm(135, digitCounter(135)) = " + (long) arm(a, digitCounter(a)));
+        System.out.println("arm(370, digitCounter(370)) = " + (long) arm(b, digitCounter(b)));
+        System.out.println("arm(12, digitCounter(12)) = " + (long) arm(c, digitCounter(c)));
         System.out.println("arm(1234567, digitCounter(1234567)) = " + (long) arm(d, digitCounter(d)));
     }
 
