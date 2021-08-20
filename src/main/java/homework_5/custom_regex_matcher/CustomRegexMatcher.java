070db1bd02af3fb35d6adafdc29665d4d8803903
pepyachka <ndrozdov9@gmail.com>
2021-08-21 00:38:17 +0300

    refactor hw5 CustomRegexMatcher

--- src/main/java/homework_5/custom_regex_matcher/CustomRegexMatcher.java
+++ src/main/java/homework_5/custom_regex_matcher/CustomRegexMatcher.java
@@ -1,38 +1,42 @@
 package homework_5.custom_regex_matcher;
 
+import base.BaseClazz;
+
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class CustomRegexMatcher {
+public class CustomRegexMatcher extends BaseClazz {
 
-    private String phoneNumber = "";
-
+    @Override
     public void run() {
         System.out.println("Enter the phone number");
-        initPhoneNumber();
-        System.out.println(isMatches());
+        System.out.println(isValid(initPhoneNumber()));
     }
 
-    private void initPhoneNumber() {
+    private String initPhoneNumber() {
+        String phoneNumber = "";
         try (Scanner scanner = new Scanner(System.in)) {
             if (scanner.hasNextLine()) {
                 phoneNumber = scanner.nextLine().trim();
             }
         }
+        return phoneNumber;
     }
 
     /*
      * String has:
-     * start with +7 or 8;
-     * first group - it is mobile operator code. Should contains three or more digit, and can wrapped in parentheses;
-     * last three groups, which contains 3, 2 and 2 digits respectively. These groups can splitted dash or space
+     * Starts with +7 or 8;
+     * First group - it is mobile operator code. It should contain three or more digits and it can be wrapped in parentheses;
+     * Last three groups should contains 3, 2 and 2 digits respectively. These groups can be splitted with the help of dash or space
      * */
-    private boolean isMatches() {
+    @Override
+    protected boolean isValid(String... arg) {
+        if (arg.length > 1) return false;
         Pattern pattern = Pattern.compile("^(\\+7|8)" +
                 "\\s?((\\((?=\\w{3}\\))\\w{3}\\))|\\w{3})\\s?" +
                 "(\\d{3})[\\-\\s]?" +
                 "(\\d{2})[\\-\\s]?" +
                 "(\\d{2})");
-        return pattern.matcher(phoneNumber).matches();
+        return pattern.matcher(arg[0]).matches();
     }
 }
