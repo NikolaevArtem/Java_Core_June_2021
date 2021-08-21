31540dcd2a47ef8a42bae3e6e03dbbab2dc11241
Allzza <allzza4279@gmail.com>
2021-08-21 11:24:44 +0300

    packages and input for hw4

---
+++ src/main/java/homework_4/custom_file_reader/CustomFileReader.java
@@ -0,0 +1,9 @@
+package homework_4.custom_file_reader;
+
+import java.io.FileInputStream;
+
+public class CustomFileReader {
+    String FILENAME = "";
+
+//    FileInputStream fileInputStream = new FileInputStream();
+}
---
+++ src/main/java/homework_4/custom_file_reader/Main.java
@@ -0,0 +1,4 @@
+package homework_4.custom_file_reader;
+
+public class Main {
+}
