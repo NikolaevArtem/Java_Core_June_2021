a85f7af33541f518c2075e25fc1c65c675f9ed41
Allzza <allzza4279@gmail.com>
2021-08-21 13:29:52 +0300

    new file.txt

--- src/main/java/homework_4/custom_file_reader/CustomFileReader.java
+++ src/main/java/homework_4/custom_file_reader/CustomFileReader.java
@@ -1,9 +1,23 @@
 package homework_4.custom_file_reader;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class CustomFileReader {
-    String FILENAME = "";
+    final static String FILE_PATH = "custom_file_reader" + File.separator + "file.txt";
 
-//    FileInputStream fileInputStream = new FileInputStream();
+
+    public static void run1() throws IOException {
+        System.out.println(FILE_PATH);
+        FileInputStream fileInputStream = new FileInputStream(FILE_PATH);
+        int i;
+
+        while((i=fileInputStream.read())!= -1){
+            System.out.print((char)i);
+        }
+    }
 }
--- src/main/java/homework_4/custom_file_reader/Main.java
+++ src/main/java/homework_4/custom_file_reader/Main.java
@@ -1,4 +1,29 @@
 package homework_4.custom_file_reader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+//        CustomFileReader customFileReader = new CustomFileReader();
+//        CustomFileReader.run1();
+
+//        URL url = new URL("https://github.com/NikolaevArtem/Java_Core_June_2021/blob/31540dcd2a47ef8a42bae3e6e03dbbab2dc11241/src/main/resources/custom_file_reader/file.txt");
+//        InputStream input = url.openStream();
+//        byte[] buffer = input.readAllBytes();
+//        String str = new String(buffer);
+//        System.out.println(str);
+
+        Path path = Path.of("src/main/resources/custom_file_reader/file.txt");
+        List<String> list = Files.readAllLines(path);
+
+        for (String str : list)
+            System.out.println(str);
+    }
+
+
 }
