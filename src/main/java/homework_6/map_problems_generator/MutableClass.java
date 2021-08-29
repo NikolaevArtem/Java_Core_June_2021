8c9e1517c7f37a0deb56f498c8810f0e51f8fcf5
Kollega1984 <bomba_25@mail.ru>
2021-08-29 19:21:21 +0300

    v6.2

--- src/main/java/homework_6/map_problems_generator/MutableClass.java
+++ src/main/java/homework_6/map_problems_generator/MutableClass.java
@@ -19,6 +19,11 @@ public class MutableClass {
     }
 
     @Override
+    public String toString() {
+        return String.valueOf(a);
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
