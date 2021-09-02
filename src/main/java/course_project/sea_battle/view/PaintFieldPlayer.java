ef6378a2185d9e08b897fc24e74c050f5ded1a70
Kollega1984 <bomba_25@mail.ru>
2021-09-02 17:12:17 +0300

    fix

--- src/main/java/course_project/sea_battle/view/PaintFieldPlayer.java
+++ src/main/java/course_project/sea_battle/view/PaintFieldPlayer.java
@@ -15,15 +15,15 @@ public class PaintFieldPlayer {
         public void paintField() {
 
             System.out.println(player.getName() + " field: ");
-//            char col = 'A';
-//            System.out.print("    ");
-//            for (int i = 0; i < 10; i++){
-//
-//                System.out.printf("%-3.7s", col);
-//
-//            }
-//            System.out.println();
-            System.out.println("    A   B   C  D   E  F   J   H  I   G");
+            char col = 'A';
+            System.out.print("   ");
+            for (int i = 0; i < 10; i++){
+
+                System.out.printf("%-4s", (char)(col+i));
+
+            }
+            System.out.println();
+            //System.out.println(" A   B   C  D   E  F   J   H  I   G");
             for (int i = 0; i < 10; i++) {
                 if (i == 9) System.out.print((i + 1) + " ");
                 else System.out.print((i + 1) + "  ");
@@ -31,7 +31,7 @@ public class PaintFieldPlayer {
                     int current = player.getPlayerFild().getField()[i][j];
                     char paint = (char) 9949;
                     if(current==5) paint = (char) 9972;
-                    System.out.print(" " + paint + " ");
+                    System.out.printf("%-3s", paint);
                 }
                 System.out.println();
             }
