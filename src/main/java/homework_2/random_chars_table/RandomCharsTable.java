d33165f50ec06e5204ddcce2a3f6ea00a828de53
Kollega1984 <bomba_25@mail.ru>
2021-09-02 16:24:59 +0300

    random fix whitespace

--- src/main/java/homework_2/random_chars_table/RandomCharsTable.java
+++ src/main/java/homework_2/random_chars_table/RandomCharsTable.java
@@ -61,13 +61,15 @@ public class RandomCharsTable {
             result += words.stream().filter(i -> i % 2 == 0)
                     .distinct()
                     .map(i -> String.valueOf((char) i.intValue()))
-                    .collect(Collectors.joining(", "));
+                    .collect(Collectors.joining(", "))
+                    .trim();
         } else {
             result = "Odd letters - ";
             result += words.stream().filter(i -> i % 2 == 1)
                     .distinct()
                     .map(i -> String.valueOf((char) i.intValue()))
-                    .collect(Collectors.joining(", "));
+                    .collect(Collectors.joining(", "))
+                    .trim();
         }
         return result;
     }
