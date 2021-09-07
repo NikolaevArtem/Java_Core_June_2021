63580b7db51dc3a1448f5ba80f0e7b663f727c08
roman <roman.s.popov@gmail.com>
2021-09-07 14:55:07 +0300

    HW6 update

--- src/main/java/homework_6/map_problems_generator/Main.java
+++ src/main/java/homework_6/map_problems_generator/Main.java
@@ -11,10 +11,10 @@ public class Main {
         map.put(key11, "firstValue");
         map.put(key12, "secondValue");
         map.put(key13, "thirdValue");
-        System.out.println("Illustration of collision");
-        System.out.println(map.get(key11));
-        System.out.println(map.get(key12));
-        System.out.println(map.get(key13));
+        System.out.println("Illustration of collision:");
+        System.out.println("Value of first key is " + map.get(key11) + ", must be firstValue");
+        System.out.println("Value of second key is " + map.get(key12) + ", must be secondValue");
+        System.out.println("Value of third key is " + map.get(key13) + ", must be thirdValue");
 
         HashMap<MapProblemsMutableGenerator, String> mutableMap = new HashMap<>();
         MapProblemsMutableGenerator key21 = new MapProblemsMutableGenerator(1, "firstProblem");
@@ -24,9 +24,9 @@ public class Main {
         mutableMap.put(key22, "secondValue");
         mutableMap.put(key23, "thirdValue");
         System.out.println("------------");
-        System.out.println("Illustration of using mutable class as a key");
-        System.out.println(mutableMap.get(key22));
+        System.out.println("Illustration of using mutable class as a key:");
+        System.out.println("Value of second key before changing is " + mutableMap.get(key22));
         key22.setProblemNumber(2);
-        System.out.println(mutableMap.get(key22));
+        System.out.println("Value of second key after changing is " + mutableMap.get(key22));
     }
 }
