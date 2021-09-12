6fc2987b95d35cb3d16d996fad4464395b79fcf6
rlrio <svetlana.kras881@gmail.com>
2021-09-12 17:18:09 +0300

    bug unit test

---
+++ src/test/java/course_project/battleship_game/utils/Constants.java
@@ -0,0 +1,6 @@
+package course_project.battleship_game.utils;
+
+public class Constants {
+    public static final String DEFAULT_PLAYER_NAME = "Player";
+    public static final String ERROR_INPUT_MESSAGE = "Wrong input.\n";
+}
