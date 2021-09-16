12d6b3b116e9e46d93bc709161996c4eb9608466
ijustwannatry <dmitrisedam11@gmail.com>
2021-09-16 09:36:19 +0300

    HW_7 edited

--- src/main/java/homework_7/kitten_to_cat_function/Main.java
+++ src/main/java/homework_7/kitten_to_cat_function/Main.java
@@ -2,13 +2,8 @@ package homework_7.kitten_to_cat_function;
 
 public class Main {
     public static void main(String[] args) {
-        KittenToCatFunction ktc = new KittenToCatFunction() {
-            @Override
-            public Cat grow(Kitten kitten) {
-                return new Cat(kitten.getAge() * 5, kitten.getName().substring(0, 3));
-            }
-        };
-
+        KittenToCatFunction ktc = kitten ->
+                new Cat(kitten.getAge() * 5, kitten.getName().substring(0, 3));
         Kitten kitten = new Kitten(1, "Kitten");
         Cat cat = ktc.grow(kitten);
         Cat anotherCat = ktc.grow(new Kitty(2, "Kotik"));
