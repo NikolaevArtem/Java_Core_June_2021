53ac68459fdd96a5c24cd728efd5866bcbd9f357
LinaFawn <tcipkoalina2000@mail.ru>
2021-09-16 03:01:25 +0300

    hm_2_pyramidPrinterFix

--- src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
+++ src/main/java/homework_2/pyramid_printer/PyramidPrinter.java
@@ -4,30 +4,33 @@ import java.util.Scanner;
 
 public class PyramidPrinter {
 
-    public void run(){
+    public void run() {
         Scanner in = new Scanner(System.in);
         System.out.println("Please input number:");
         String inpt = in.nextLine();
-        if(isValid(inpt)){
-            int n = Integer.parseInt(inpt);
-            if (n == 0)
-                System.out.println("n = 0");
-            else {
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j <= i; j++)
-                        System.out.print("x");
-                    System.out.println();
-                }
+        if (isValid(inpt)) {
+            try {
+                int n = Integer.parseInt(inpt);
+                printResult(n);
+            } catch (NumberFormatException e) {
+                System.out.println(e);
             }
-
-        } else{
+        } else
             System.out.println("Only 1 non-negative integer integer is allowed as passed parameter");
-
-        }
     }
 
-    public boolean isValid(String inpt){
-        return inpt.matches("^-?\\d+$");
+    public boolean isValid(String inpt) {
+        return inpt.chars().allMatch(Character::isDigit);
+    }
 
+    public static void printResult(int n) {
+        if (n == 0)
+            System.out.println(" ");
+        else
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <= i; j++)
+                    System.out.print("x");
+                System.out.println();
+            }
     }
 }
