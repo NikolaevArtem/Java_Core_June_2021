10d4d1b7c40f7de2c1c16b827f0b19ec75a7b250
Daisy <la.reine.m@gmai.com>
2021-09-19 18:32:32 +0300

    Finished Immutable class

--- src/main/java/Homework3/Immutable_Class_Task/Employee.java
+++ src/main/java/Homework3/Immutable_Class_Task/Employee.java
@@ -2,23 +2,6 @@ package Homework3.Immutable_Class_Task;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
-Requirements for immutable classes
-1. The class must be declared as final/
-
-2.Fields in the class must be declared as private
-
-3.Data members in the class must be declared as final
-(So that we can’t change the value of it after object creation)
-
-4. A parameterized constructor should initialize all the fields performing a deep copy
-(So that data members can’t be modified with object reference)
-5.Deep Copy of objects should be performed in the getter methods
-(To return a copy rather than returning the actual object reference)
-
-6. No setters
- */
-
 public final class Employee {
         private final String name;
         private final int regNo;
@@ -37,7 +20,7 @@ public final class Employee {
             this.metadata = tempMap;
         }
 
-        public String getName() { return name; }
+        public String getName() { return new String(this.name); }
 
         public int getRegNo() { return regNo; }
 
