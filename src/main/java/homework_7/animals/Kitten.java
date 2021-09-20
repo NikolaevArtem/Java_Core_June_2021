72a80950cdf5e33b10e6fb095079af5b9e3b9f3b
Gamzat Asadulaev <asadulaevgamzat@gmail.com>
2021-09-20 09:05:38 +0300

    homework 7

---
+++ src/main/java/homework_7/animals/Kitten.java
@@ -0,0 +1,14 @@
+package homework_7.animals;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Kitten {
+    private String name;
+    private int age;
+    private double weight;
+
+}
