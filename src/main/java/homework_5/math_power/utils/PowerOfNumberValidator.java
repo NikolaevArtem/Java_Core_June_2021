34b0d61c448928b7118fb745c1f0e1079b1a7e62
rlrio <svetlana.kras881@gmail.com>
2021-09-20 17:36:34 +0300

    hw5

--- src/main/java/homework_5/math_power/utils/PowerOfNumberValidator.java
+++ src/main/java/homework_5/math_power/utils/PowerOfNumberValidator.java
@@ -2,7 +2,7 @@ package homework_5.math_power.utils;
 
 public final class PowerOfNumberValidator {
 
-    public static boolean isValid(String[] args) throws NumberFormatException {
+    public static boolean isValid(String[] args) {
         try {
             return args.length == 2 && Integer.parseInt(args[0]) >= 0 && Integer.parseInt(args[1]) >= 0;
         } catch (NumberFormatException e) {
