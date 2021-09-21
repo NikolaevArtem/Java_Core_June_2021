c5de362659660fcbea4fb183072b199bf34b001c
Waranko <mari.waranko@gmail.com>
2021-09-21 17:22:30 +0300

    SeaBattle interface fix

--- src/main/java/course_project/services/GameService.java
+++ src/main/java/course_project/services/GameService.java
@@ -9,56 +9,72 @@ import java.util.List;
 
 public class GameService {
 
+    private final String WELCOME_MESSAGE = "Welcome to Sea Battle! You are playing with the computer now!";
+    private final String USER_WIN_MESSAGE = "You won! Congratulations!!!";
+    private final String COMPUTER_WIN_MESSAGE = "Computer won!";
+
     public void run() {
         UserService userService = new UserService();
-        userService.printWelcomeMessage();
+        System.out.println(WELCOME_MESSAGE);
         GameData data = new GameData(new ShipLocationGenerator());
         processMoves(userService, data);
     }
 
     private void processMoves(UserService userService, GameData data) {
-        boolean gotWinner = false;
         List<Move> lastHit = new ArrayList<>();
-        while (!gotWinner) {
-            Response response;
-            do {
-                response = processUserMove(userService, data);
-                userService.printComputerResponse(response);
-                if (response == Response.KILLED) {
-                    if (data.isComputerFleetEmpty()) {
-                        userService.printUserWinMessageAndExit();
-                        gotWinner = true;
-                        break;
-                    }
-                }
-            } while (response == Response.HIT || response == Response.KILLED || response == Response.VISITED);
-            if (gotWinner) break;
-            do {
-                Move nextMove = MoveGenerator.generate(data.getUserField(), lastHit);
-                response = processComputerMove(userService, nextMove);
-                if (response == Response.HIT || response == Response.KILLED) {
-                    lastHit.add(nextMove);
-                }
-                data.saveComputerMoveResult(response, nextMove, lastHit);
-                if (response == Response.KILLED) {
-                    lastHit.clear();
-                    if (data.isUserFleetEmpty()) {
-                        userService.printComputerWinMessageAndExit();
-                        data.getComputerField().printField();
-                        gotWinner = true;
-                        break;
-                    }
-                }
-            } while (response == Response.HIT || response == Response.KILLED);
+        boolean isUsersTurn = true;
+        while (!isGameFinished(data)) {
+            if (isUsersTurn) {
+                processUserMoves(userService, data);
+                isUsersTurn = false;
+            } else {
+                processComputerMoves(userService, data, lastHit);
+                isUsersTurn = true;
+            }
         }
+        finishGame(data, userService);
     }
 
-    private Response processComputerMove(UserService userService, Move move) {
-        return userService.completeComputerMove(move);
+    private void processUserMoves(UserService userService, GameData gameData) {
+        Response response;
+        do {
+            response = gameData.getResponseFromComputer(userService.getMove());
+            userService.printComputerResponse(response);
+            if (response == Response.KILLED) {
+                if (isGameFinished(gameData)) return;
+            }
+        } while (response == Response.HIT || response == Response.KILLED || response == Response.VISITED);
     }
 
-    private Response processUserMove(UserService userService, GameData gameData) {
-        return gameData.getResponseFromComputer(userService.getMove());
+    private void processComputerMoves(UserService userService, GameData data, List<Move> lastHit) {
+        Response response;
+        do {
+            Move nextMove = MoveGenerator.generate(data.getUserField(), lastHit);
+            response = userService.completeComputerMove(nextMove);
+            if (response == Response.HIT || response == Response.KILLED) {
+                lastHit.add(nextMove);
+            }
+            data.saveComputerMoveResult(response, nextMove, lastHit);
+            if (response == Response.KILLED) {
+                lastHit.clear();
+                if (isGameFinished(data)) return;
+            }
+        } while (response == Response.HIT || response == Response.KILLED);
+    }
+
+    private boolean isGameFinished(GameData data) {
+        return data.isComputerFleetEmpty() || data.isUserFleetEmpty();
+    }
+
+    private void finishGame(GameData data, UserService userService) {
+        if (data.isUserFleetEmpty()) {
+            System.out.println(COMPUTER_WIN_MESSAGE);
+            data.getComputerField().printField();
+        }
+        if (data.isComputerFleetEmpty()) {
+            System.out.println(USER_WIN_MESSAGE);
+        }
+        userService.closeBuffer();
     }
 
 }
--- src/main/java/course_project/services/UserService.java
+++ src/main/java/course_project/services/UserService.java
@@ -7,6 +7,13 @@ import java.io.IOException;
 
 public class UserService {
 
+    private final String MOVE_MESSAGE = "Make your move (e. g. D4): ";
+    private final String RESPONSE_MESSAGE = "   Enter 0, you have no ship on this cell,\n" +
+            "   Enter 1, if part of your ship is on this cell,\n" +
+            "   Enter 2, if there is last/only not killed part os your ship.\n" +
+            "   Enter a response (a digit from 0 to 2):";
+    private final String VISITED_ALREADY_MESSAGE = "You have checked this cell already! Try to make a move again.";
+    private final String WRONG_MOVE_FORMAT_MESSAGE = "Wrong input format. Try again:";
     private final InputService inputService;
 
     public UserService() {
@@ -14,21 +21,22 @@ public class UserService {
     }
 
     public Move getMove() {
-        printMoveMessage();
+        System.out.println(MOVE_MESSAGE);
         String input = inputService.getInput();
+        input = input.toUpperCase();
         while (!isValidMove(input)) {
-            System.out.println("Wrong input format. Try again:");
+            System.out.println(WRONG_MOVE_FORMAT_MESSAGE);
             input = inputService.getInput();
         }
         return new Move(input);
     }
 
     public Response completeComputerMove(Move move) {
-        printMove(move);
-        printResponseMessage();
+        System.out.println("Computer's move: " + move);
+        System.out.println(RESPONSE_MESSAGE);
         String input = inputService.getInput();
         while (!isValidMoveResponse(input)) {
-            System.out.println("Wrong input format. Try again:");
+            System.out.println(WRONG_MOVE_FORMAT_MESSAGE);
             input = inputService.getInput();
         }
         return Response.values()[Integer.parseInt(input)];
@@ -50,40 +58,16 @@ public class UserService {
 
     public void printComputerResponse(Response response) {
         if (response == Response.VISITED) {
-            printVisitedAlreadyMessage();
+            System.out.println(VISITED_ALREADY_MESSAGE);
         } else {
-            System.out.println(response);
+            if (response == Response.MISSED) {
+                System.out.println("Computer has no ship there. It's computer's turn to make a move.");
+            } else if (response == Response.HIT) {
+                System.out.println("You have hit the computer's ship. It's your turn ta make a move again!");
+            } else {
+                System.out.println("You have destroyed one of computer's ships entirely. It's your turn ta make a move again!");
+            }
         }
-    }
-
-    public void printMove(Move move) {
-        System.out.println("Computer move: " + move);
-    }
-
-    public void printWelcomeMessage() {
-        System.out.println("Welcome to Sea Battle! You do the first move!");
-    }
-
-    public void printMoveMessage() {
-        System.out.println("Make your move (e. g. D4): ");
-    }
-
-    public void printResponseMessage() {
-        System.out.println("0 - missed, 1 - hit, 2 - killed.\nEnter a response:");
-    }
-
-    public void printVisitedAlreadyMessage() {
-        System.out.println("You have checked this cell already! Try to make a move again.");
-    }
-
-    public void printUserWinMessageAndExit() {
-        System.out.println("You won! Congratulations!!!");
-        closeBuffer();
-    }
-
-    public void printComputerWinMessageAndExit() {
-        System.out.println("Computer won!");
-        closeBuffer();
     }
 
     public void closeBuffer() {
