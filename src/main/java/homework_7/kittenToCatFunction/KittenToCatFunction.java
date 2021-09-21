9d3d8359eef00393a955d851ca2495a6248aa82f
Tom Shandrikov <ashandrikov@gmail.com>
2021-09-21 17:24:04 +0300

    Small bug fixed.

--- src/main/java/homework_7/kittenToCatFunction/KittenToCatFunction.java
+++ src/main/java/homework_7/kittenToCatFunction/KittenToCatFunction.java
@@ -1,6 +1,6 @@
 package homework_7.kittenToCatFunction;
 
 @FunctionalInterface
-public interface KittenToCatFunction{
+public interface KittenToCatFunction<C extends Cat, K extends Kitten>{
     Cat grow(Kitten k);
 }
