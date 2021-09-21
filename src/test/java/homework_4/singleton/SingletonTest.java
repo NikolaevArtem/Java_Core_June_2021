71662a49aac00aedbf91ca1851d2c5cf0c06a82f
kirillfilipcik <kirill.filipchik@yandex.ru>
2021-09-21 13:47:16 +0300

    Homework fix SingletonTest

--- src/test/java/homework_4/singleton/SingletonTest.java
+++ src/test/java/homework_4/singleton/SingletonTest.java
@@ -12,5 +12,6 @@ class SingletonTest {
         Singleton singleton2 = Singleton.getInstance();
 
         assertEquals(singleton1, singleton2);
+        assertTrue(singleton1 == singleton2);
     }
 }
