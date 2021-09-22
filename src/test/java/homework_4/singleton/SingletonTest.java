3a5ed1b6d6d03dcbec93cb589b7d3894535b8f26
Ozone_TaTaY <volchenkovolk@mail.ru>
2021-09-22 19:10:44 +0300

    Fix tests for HW_4

--- src/test/java/homework_4/singleton/SingletonTest.java
+++ src/test/java/homework_4/singleton/SingletonTest.java
@@ -3,6 +3,7 @@ package homework_4.singleton;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SingletonTest {
     @Test
@@ -10,5 +11,6 @@ public class SingletonTest {
         Singleton singleton1 = Singleton.getInstance("I am 1");
         Singleton singleton2 = Singleton.getInstance("I am 2");
         assertEquals(singleton1, singleton2);
+        assertTrue(singleton1 == singleton2);
     }
 }
